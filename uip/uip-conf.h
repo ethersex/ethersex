67675e1aa4036902d129fc9e8a71d598f1385a60
Stefan Siegl <stesie@mohawk.metafnord.de>
2007-08-12 03:07:02 +0200

    reduce number of max. connections to 3

--- uip/uip-conf.h
+++ uip/uip-conf.h
@@ -39,7 +39,7 @@ typedef unsigned short uip_stats_t;
  *
  * \hideinitializer
  */
-#define UIP_CONF_MAX_CONNECTIONS 4
+#define UIP_CONF_MAX_CONNECTIONS 3
 
 /**
  * Maximum number of listening TCP ports.
