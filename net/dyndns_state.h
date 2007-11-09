6da34ff101acc2998507ee6d18f5cd48e482407a
Stefan Siegl <stesie@brokenpipe.de>
2007-11-09 20:46:58 +0100

    add dyndns_state.h (sorry for accidentally having fucked up previous commit)

---
+++ net/dyndns_state.h
@@ -0,0 +1,31 @@
+/* vim:fdm=marker ts=4 et ai
+ * {{{
+ *
+ * Copyright (c) 2007 by Christian Dietrich <stettberger@dokucode.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * For more information on the GPL, please go to:
+ * http://www.gnu.org/copyleft/gpl.html
+ }}} */
+
+#ifndef DYNDNS_STATE_H
+#define DYNDNS_STATE_H
+
+struct dyndns_connection_state_t {
+  uint8_t state;
+};
+
+#endif
