8f3d80f768f3e4a29e28b3a026c523130718b786
Jochen Roessner <jochen@fileserver.lugrot.de>
2008-02-17 22:35:23 +0100

    sensormodul: skript funktionen an den aktuellen stand angepasst

---
+++ contrib/sensormodul/sensor.py
@@ -0,0 +1,202 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# ethersex rfm12 testprogramm
+import socket
+import sys
+import binascii, string, optparse, math, time
+
+#HOST = 'sensor1.dyn.metafnord.de'    # ethersex host
+PORT = 11587             # sensor port
+s = None
+CONDITION_RED = 1
+CONDITION_YELLOW = 3
+CONDITION_OFF = 0
+CONDITION_GREEN = 2
+LED = ( "OFF","RED","GREEN","YELLOW" )
+
+
+parser = optparse.OptionParser()
+parser.add_option("-v", "--verbose",
+                  action="store_true", dest="verbose", default=False,
+                  help="Debug output")
+parser.add_option("-H", "--host", dest="host", default='sensor1.dyn.metafnord.de',
+                  help="Sleep Time for the Log")
+parser.add_option("-1", "--oneline",
+                  action="store_true", dest="oneline", default=False,
+                  help="Output all to one Line")
+parser.add_option("-r", "--raw",
+                  action="store_true", dest="raw", default=False,
+                  help="Output Sensor raw direct data")
+parser.add_option("-l", "--log",
+                  action="store_true", dest="log", default=False,
+                  help="Run forever")
+parser.add_option("-s", "--sleep", dest="sleep", default=10,
+                  help="Sleep Time for the Log")
+parser.add_option("-t", "--text", dest="text", help="Send Text to the LCD")
+parser.add_option("-L", "--led", dest="led", help="Set LED on Modul")
+parser.add_option("-R", "--red", dest="max_red", help="Set Max for red LED")
+parser.add_option("-Y", "--yellow", dest="max_yellow", help="Set Max for yellow LED")
+parser.add_option("-O", "--off", dest="max_off", help="Set Max for LED off")
+parser.add_option("-G", "--green", dest="max_green", help="Set Max for green LED")
+parser.add_option("-D", "--diff", dest="hysteresis", help="Set Hysteresis difference")
+(options, args) = parser.parse_args()
+
+
+for res in socket.getaddrinfo(options.host, PORT, socket.AF_UNSPEC, socket.SOCK_DGRAM):
+	af, socktype, proto, canonname, sa = res
+	try:
+		s = socket.socket(af, socktype, proto)
+	except socket.error, msg:
+		s = None
+		continue
+	try:
+		s.connect(sa)
+	except socket.error, msg:
+		s.close()
+		s = None
+		continue
+	break
+if s is None:
+	print 'could not open socket'
+	sys.exit(1)
+#052302f114
+s.settimeout(5)
+
+log=1
+while log:
+
+	while 1:
+		if (not options.log and options.text and str(options.text) != ""):
+			print "Sende Text:%s" % str(options.text)
+			s.send(":"+str(options.text))
+		elif(not options.log and options.led):
+			#print options.led
+			print "Set LED to %s" % LED[int(options.led)]
+			s.send("!"+str(options.led))
+		elif(options.max_red):
+				print "Set max for red to %03i" % int(options.max_red)
+				s.send("1%03i" % int(options.max_red))
+				todel = options.max_red
+		elif(options.max_yellow):
+				print "Set max for yellow to %03i" % int(options.max_yellow)
+				s.send("2%03i" % int(options.max_yellow))
+		elif(options.max_off):
+				print "Set max for off to %03i" % int(options.max_off)
+				s.send("3%03i" % int(options.max_off))
+		elif(options.max_green):
+				print "Set max for green to %03i" % int(options.max_green)
+				s.send("4%03i" % int(options.max_green))
+		elif(options.hysteresis):
+				print "Set hysteresis to %03i" % int(options.hysteresis)
+				s.send("5%03i" % int(options.hysteresis))
+		else:
+			s.send(" ")
+
+		try:
+			data = s.recv(1024)
+		except:
+			data = ""
+			sys.stdout.write(".")
+			#print "Network read timeout"
+		
+		if(len(data) > 4):
+			if(not options.log and (options.max_red or options.max_yellow or options.max_off or options.max_green or options.hysteresis)):
+				if (options.max_red):
+					options.max_red = None
+				elif (options.max_yellow):
+					options.max_yellow = None
+				elif (options.max_off):
+					options.max_off = None
+				elif (options.max_green):
+					options.max_green = None
+				elif (options.hysteresis):
+					options.hysteresis = None
+				continue
+			else:
+				break
+	
+	rawdata = ""
+	for i in range(0, 32, 8):
+		for i2 in range(2, 7):
+			#print data[i+i2], i+i2
+			rawdata = rawdata + data[i+i2]
+	
+	if (options.verbose):
+		print 'Received', len(data), binascii.hexlify(data)
+		print data
+	
+	grenzwert = {}
+	grenzwert["red"] = ord(data[32])
+	grenzwert["yellow"] = ord(data[33])
+	grenzwert["off"] = ord(data[34])
+	grenzwert["green"] = ord(data[35])
+	grenzwert["hysteresis"] = ord(data[36])
+	statusbyte = ord(data[37])
+	
+	if (options.verbose):
+		print "red:%i yellow:%i off:%i green:%i hysteresis:%i status(hex):%02X" % (grenzwert["red"], grenzwert["yellow"], grenzwert["off"], grenzwert["green"], grenzwert["hysteresis"], statusbyte)
+	
+	aT = -167.123
+	bT = 0.275501
+	cT = -0.000102316
+	dT  = 1.92025e-08
+	
+	temperaturcalc = {}
+	
+	for i in range(0, 2):
+		irel=i*8
+		wert = ord(data[irel+1]) * 256 + ord(data[irel])
+		if (options.verbose):
+			print wert
+		if (wert < 1023):
+			volt = (2.5/1023.0)* wert
+			R = 2200.0/(5.0 - volt)*volt
+			temperaturcalc[i] = aT + R*( bT + R*( cT + R*dT))
+			if (not options.oneline):
+				print "Temp %2.1f °C Wert %i" % ( temperaturcalc[i],wert )
+			
+	
+	i=24
+	wert = ord(data[i+1]) * 256 + ord(data[i])
+	feuchte = 100.0 / 1024.0 * wert
+	
+	if (not options.oneline):
+		print ("Akt.Feuchte %2.1f %%" % (feuchte))
+	
+	Reltemp_raum = 6.1078 * math.exp(((7.5*temperaturcalc[0])/(237.3+temperaturcalc[0]))/0.434294481903252)
+	Reltemp_wand = 6.1078 * math.exp(((7.5*temperaturcalc[1])/(237.3+temperaturcalc[1]))/0.434294481903252)
+	Maxfeuchte = Reltemp_wand / Reltemp_raum * 100.0
+	
+	Taupunkt = (237.3 * math.log(feuchte/100*6.1078*math.exp(((7.5*temperaturcalc[0])/(237.3+temperaturcalc[0]))/0.434294481903252)/6.1078,10)) / (7.5 -math.log(feuchte/100*6.1078*math.exp(((7.5*temperaturcalc[0])/(237.3+temperaturcalc[0]))/0.434294481903252)/6.1078,10))
+	
+	if(feuchte > (Maxfeuchte - (grenzwert["red"]/10))):
+		akt_ledcondition = CONDITION_RED
+	elif (feuchte > (Maxfeuchte - (grenzwert["yellow"]/10))):
+		akt_ledcondition = CONDITION_YELLOW
+	elif (feuchte > (Maxfeuchte - (grenzwert["off"]/10))):
+		akt_ledcondition = CONDITION_OFF
+	elif (feuchte < (Maxfeuchte - (grenzwert["green"]/10))):
+		akt_ledcondition = CONDITION_GREEN
+		
+	
+	if (not options.oneline):
+		print ("Max.Feuchte %2.1f %% Taupunkt %2.1f °C LED: %s" % (Maxfeuchte, Taupunkt, LED[akt_ledcondition]))
+		if (options.raw):
+			print "rawdata: %s" % rawdata
+			print "red:%i yellow:%i off:%i green:%i hysteresis:%i status(hex):%02X" % (grenzwert["red"], grenzwert["yellow"], grenzwert["off"], grenzwert["green"], grenzwert["hysteresis"], statusbyte)
+
+	else:
+		if (options.raw):
+			print "Temp.Raum %2.1f °C Temp.Wand %2.1f °C Taupunkt %2.1f °C Feuchte %2.1f %% Max.Feuchte %2.1f %% LED:%s %s Gr:%i,%i,%i,%i h:%i s:%02X" % ( temperaturcalc[0],temperaturcalc[1], Taupunkt,feuchte,Maxfeuchte, LED[akt_ledcondition] ,rawdata, grenzwert["red"], grenzwert["yellow"], grenzwert["off"], grenzwert["green"], grenzwert["hysteresis"], statusbyte)
+		else:
+			print "Temp.Raum %2.1f °C Temp.Wand %2.1f °C Taupunkt %2.1f °C Feuchte %2.1f %% Max.Feuchte %2.1f %% LED:%s" % ( temperaturcalc[0],temperaturcalc[1], Taupunkt,feuchte,Maxfeuchte,LED[akt_ledcondition])
+
+	if (options.log):
+		time.sleep(float(options.sleep))
+	else:
+		log = 0
+
+s.close()
+
+
