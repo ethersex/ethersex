eafc16016fc9ae52b36fb697b829194717b71746
Jochen Roessner <jochen@lugrot.de>
2008-02-28 17:34:26 +0100

    rfm12: fixed bug, rfm12 code auch nur eincompilieren wenn RFM12_SUPPORT definiert ist

--- timer.c
+++ timer.c
@@ -205,11 +205,12 @@ void timer_process(void)
 		fill_llh_and_transmit();
 #           endif /* NTP_SUPPORT */
         }
-          if (counter == 300 ) {
-            rfm12_t_status = rfm12_trans(0x0000); /*get the status Register from the RFM12*/
-            /* FIXME do anything when rfm12 kommunication hangs */
-          }
-
+#       ifdef RFM12_SUPPORT
+        if (counter == 300 ) {
+          rfm12_t_status = rfm12_trans(0x0000); /*get the status Register from the RFM12*/
+          /* FIXME do anything when rfm12 kommunication hangs */
+        }
+#       endif
         /* expire arp entries every 10 seconds */
         if (counter == 500) {
 #           ifdef ENC28J60_SUPPORT
