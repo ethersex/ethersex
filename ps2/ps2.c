40b268b466e2d8b24a891a0743d4dc8f8be450f7
Christian Dietrich <stettberger@dokucode.de>
2008-03-24 00:31:11 +0000

    ps2: add comments and fixup some code

--- ps2/ps2.c
+++ ps2/ps2.c
@@ -50,6 +50,10 @@ uint8_t send_next = 0;
 
 struct key_press key;
 
+/* This is the keymap
+ * every byte from the keyboard is connected to a character
+ * the LC macro is used to generate german and us layouts 
+ */
 static uint8_t PROGMEM keycodes[] = {
   0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00, /* keycode 00-07 */
   0x00,0x00,0x00,0x00,0x00,0x00, LC('`','^'),0x00, /* keycode 08-0f */
@@ -68,7 +72,7 @@ static uint8_t PROGMEM keycodes[] = {
    '0', ',', '2', '5', '6', '8',0x00,0x00, /* keycode 70-77 */
   0x00, '+', '3', '-', '*', '9',0x00,0x00, /* keycode 78-7f	*/
 };
-
+/* And the keymap with pressed shift key */
 static uint8_t PROGMEM keycodes_shift[] = {
   0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00, /* keycode 00-07 */
   0x00,0x00,0x00,0x00,0x00,0x00,LC('~',0x00),0x00, /* keycode 08-0f */
@@ -95,6 +99,9 @@ ps2_init(void)
   parity = 1;
   key.num = 0;
 
+  /* Enable the interrupt for the clock pin, because the keyboard is clocking
+   * us, not vice versa 
+   */
   PCICR |= _BV(PS2_PCIE);
   PS2_PCMSK |= PIN_BV(PS2_CLOCK);
 
@@ -107,6 +114,9 @@ ps2_init(void)
 void
 ps2_periodic(void)
 {
+  /* when we loose a bit we are out of sync, so we want a timeout for
+   * recieving bytes
+   */
   if(timeout--)
     if(timeout == 0) {
       /* Reset the reciever */
@@ -118,7 +128,7 @@ ps2_periodic(void)
 void
 ps2_send_byte(uint8_t byte)
 {
-  cli();
+  uint8_t sreg = SREG; cli();
   uint8_t i = 11;
   DDR_CONFIG_OUT(PS2_DATA);
   DDR_CONFIG_OUT(PS2_CLOCK);
@@ -126,12 +136,16 @@ ps2_send_byte(uint8_t byte)
   PIN_CLEAR(PS2_CLOCK);
   while (i--)
     _delay_us(10);
+
   /* data low */
   PIN_CLEAR(PS2_DATA);
+
   /* clock high */
   PIN_SET(PS2_CLOCK);
+
   /* Wait until clock is low again */
   DDR_CONFIG_IN(PS2_CLOCK);
+
   while (PIN_HIGH(PS2_CLOCK));
   uint8_t parity = 1;
   bitcount = 0;
@@ -159,11 +173,12 @@ ps2_send_byte(uint8_t byte)
   DDR_CONFIG_IN(PS2_DATA);
   PIN_CLEAR(PS2_DATA);
   PIN_CLEAR(PS2_CLOCK);
+
   /* Wait for the ack from the keyboard */
   while (PIN_HIGH(PS2_DATA));
   while (PIN_HIGH(PS2_CLOCK));
 
-  sei();
+  SREG = sreg;
 }
 
 static void
@@ -173,9 +188,11 @@ decode_key(uint8_t keycode)
   case KEY_ALT:
     key.alt = 0;
     break;
+
   case KEY_CTRL:
     key.ctrl = 0;
     break;
+
   case KEY_SHIFT_RIGHT:
   case KEY_SHIFT_LEFT:
     key.shift = 0;
@@ -183,28 +200,36 @@ decode_key(uint8_t keycode)
     ps2_send_byte(PS2_SET_LED);
     ps2_send_byte(leds);
     break;
-  case KEY_LIN:
+
+  case KEY_LIN: /* The LIN key is also known ans windos key */
     key.lin = 0;
     break;
-  case KEY_NUM_LOCK:
+
+  case KEY_NUM_LOCK: /* num lock toggles the num lock flag */
     key.num ^= 1;
     leds ^= PS2_NUM_LOCK_LED; 
     ps2_send_byte(PS2_SET_LED);
     ps2_send_byte(leds);
     key.lin = 0;
     break;
+
   case KEY_CAPS_LOCK:
     key.shift = 1;
     leds |= PS2_CAPS_LOCK_LED; 
     ps2_send_byte(PS2_SET_LED);
     ps2_send_byte(leds);
     break;
+
   default:
+#ifdef SYSLOG_SUPPORT
+    /* For debugging purposes we send the keycode via syslog */
     if (key.extended && keycode == 0x6c)
       syslog_send_P(PSTR("HOME"));
+
     syslog_sendf("Key: %x %c", data, key.shift 
                  ? pgm_read_byte(&keycodes_shift[keycode])
                  : pgm_read_byte(&keycodes[keycode]));
+#endif
     break;
   }
 }
@@ -216,6 +241,7 @@ SIGNAL(PS2_INTERRUPT)
     if (bitcount == 11) 
       timeout = 2;
 
+    /* the bits 10 to 3 are our data byte */
     if (bitcount < 11 && bitcount > 2) {
       data >>= 1;
       if ( PIN_HIGH(PS2_DATA)) {
@@ -223,35 +249,48 @@ SIGNAL(PS2_INTERRUPT)
         parity ^= 1;
       }
     } else if (bitcount == 2) {
+      /* check the parity, and drop the byte if it doesn't match */
       if (((PIN_HIGH(PS2_DATA)) ^ (parity << PIN_NR(PS2_DATA))) != 0) {
         data = 0;
       }
     }
 
     if (--bitcount == 0) {
+      /* the is up flag is set, when the button was released, after the is_up
+       * byte, the keycode is repeated once again 
+       */
       if (is_up) {
         decode_key(data);
         is_up = 0;
       } else if (data == 0xF0 && ! is_up) 
         is_up = 1;
       else {
+        /* Some codes must be processed, while they are hold down */
         switch(data) {
         case KEY_ALT:
           key.alt = 1;
           break;
+
         case KEY_CTRL:
           key.ctrl = 1;
           break;
+
         case KEY_SHIFT_RIGHT:
         case KEY_SHIFT_LEFT:
           key.shift = 1;
           break;
+
         case KEY_LIN:
           key.lin = 1;
           break;
+
+        /* The NUMPAD is attached via a extended keymap, we use a flag for
+         * this
+         */
         case 0xE0:
           key.extended = 1;
           break;
+
         default:
           key.extended = 0;
           break;
