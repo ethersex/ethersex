090942a240614d16a629155388210cf3b3dba4bd
Jochen Roessner <jochen@lugrot.de>
2008-05-08 18:55:49 +0200

    zbus: fix fehlender rxstart wenn skipjack das paket verwirft

--- zbus/zbus.c
+++ zbus/zbus.c
@@ -35,6 +35,7 @@
 #include "../crypto/encrypt-llh.h"
 #include "../net/zbus_raw_net.h"
 #include "zbus.h"
+#include "blink.c"
 
 static uint8_t send_escape_data = 0;
 static uint8_t recv_escape_data = 0;
@@ -120,6 +121,8 @@ zbus_rxfinish(void)
     if (!zbus_raw_conn->rport)
 #endif
     zbus_decrypt(recv_ctx.data, &recv_ctx.len);
+    if(!recv_ctx.len)
+      zbus_rxstart ();
 #endif
     return (struct zbus_ctx *) (&recv_ctx);
   }
