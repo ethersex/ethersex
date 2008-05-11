214b7f660f9f5f4fc639b4cadaa45ffd15754a96
Stefan Siegl <stesie@brokenpipe.de>
2008-05-11 16:48:41 +0200

    defaults.mk: compile with '-Wall -W -Wno-unused-parameter' by default.

--- defaults.mk
+++ defaults.mk
@@ -19,7 +19,7 @@ export HOSTCC
 
 # flags for the compiler
 CPPFLAGS += -mmcu=$(MCU) -DF_CPU=$(F_CPU) 
-CFLAGS += -g -Os -std=gnu99
+CFLAGS += -g -Os -std=gnu99 -Wall -W -Wno-unused-parameter
 
 # flags for the linker
 LDFLAGS += -mmcu=$(MCU)
