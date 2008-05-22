647788c31eda5cfeca7b5242361f63b457c3bf82
Stefan Siegl <stesie@wuchtbrumme.metafnord.de>
2008-05-22 16:02:47 +0200

    rules.mk: clean away *.E and *.s also.

--- rules.mk
+++ rules.mk
@@ -7,7 +7,7 @@ OBJECTS += $(patsubst %.c,%.o,${SRC})
 #	$(STRIP) --strip-unneeded $@
 
 clean-common:
-	$(RM) $(TARGET) *.[oda] *.d.new *~
+	$(RM) $(TARGET) *.[odasE] *.d.new *~
 
 clean: clean-common
 
