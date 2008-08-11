93e9d8cca382a9469097f550f5121627f0e6b3a9
Guido Pannenbecker <info@sd-gp.de>
2008-08-11 15:47:38 +0200

    mcuf: blp, eblp and mcuf-udp-packets are now workin :)

--- mcuf/mcuf.c
+++ mcuf/mcuf.c
@@ -61,8 +61,16 @@ struct mcuf_packet {
 struct eblp_packet {
   uint16_t magic[2];
   uint32_t framenumber;
-  uint16_t height;
   uint16_t width;
+  uint16_t height;
+  uint8_t data[];
+};
+
+struct blp_packet {
+  uint16_t magic[2];
+  uint32_t framenumber;
+  uint16_t width;
+  uint16_t height;
   uint8_t data[];
 };
 
@@ -92,13 +100,26 @@ mcuf_newdata(void)
   /* If we send a packet, drop the new packet */
   if (buffer.sent < buffer.len) return;
 
+
   /* MCUF Magic bytes - see https://wiki.blinkenarea.org/index.php/MicroControllerUnitFrame */
   if ( strncmp(uip_appdata, "\x23\x54\x26\x66", 4) == 0) {
     /* input */
     struct mcuf_packet *pkt = (struct mcuf_packet *)uip_appdata;
+    uint16_t height = htons(pkt->height);
+    uint16_t width = htons(pkt->width);
+    uint16_t channels = htons(pkt->channels);
+    if (channels < 1) {
+#ifdef SYSLOG_SUPPORT
+      syslog_sendf("Warning: forced channels of MCUF-Frame to 1 (orig value: %d)", channels);
+#endif
+      channels = 1;
+    }
+    uint16_t maxval = htons(pkt->maxval);
     
-    if ( pkt->maxval != 256 ) {
-      syslog_sendf("Warning: skiped MCUF-Frame cause of wrong maxval: %d", pkt->maxval);
+    if ( maxval != 255 && !( maxval == 1 && channels == 1  ) ) {
+#ifdef SYSLOG_SUPPORT
+      syslog_sendf("Warning: skiped MCUF-Frame cause of wrong maxval: %d", maxval);
+#endif
       return;
     }
     
@@ -107,24 +128,30 @@ mcuf_newdata(void)
 
     /* write frame-data to output-buffer */
     uint8_t x, y;
-    if (pkt->channels == 3) {
+    if (channels == 3) {
       /* Handle RGB-Frames */
+#ifdef SYSLOG_SUPPORT
+      //syslog_send_P(PSTR("Notice: Using avg of 3-channels-mcuf-frame"));
+#endif
       for (y = 0; y < 8; y++) {
         for (x = 0; x < 18; x++) {
-          if (pkt->height > y && pkt->width > x) {
-            uint8_t red   = pkt->data[(x + (y * pkt->width)) * pkt->channels + 0];
-            uint8_t green = pkt->data[(x + (y * pkt->width)) * pkt->channels + 1];
-            uint8_t blue  = pkt->data[(x + (y * pkt->width)) * pkt->channels + 2];
+          if (height > y && width > x) {
+            uint8_t red   = pkt->data[(x + (y * width)) * channels + 0];
+            uint8_t green = pkt->data[(x + (y * width)) * channels + 1];
+            uint8_t blue  = pkt->data[(x + (y * width)) * channels + 2];
             buffer.data[12 + (x + (y * 18))] = (red + green + blue) / 3;
           }
         }
       }
     } else {
       /* for non-RGB-Frames use only channel 1 and ignore all others */
+#ifdef SYSLOG_SUPPORT
+      //syslog_sendf("Notice: Using Channel 1 of MCUF-Frame (channels in Frame: %d)", channels);
+#endif
       for (y = 0; y < 8; y++) {
         for (x = 0; x < 18; x++) {
-          if (pkt->height > y && pkt->width > x) {
-            buffer.data[12 + (x + (y * 18))] = pkt->data[(x + (y * pkt->width)) * pkt->channels + 0 ];
+          if (height > y && width > x) {
+            buffer.data[12 + (x + (y * 18))] = pkt->data[(x + (y * width)) * channels + 0 ];
           }
         }
       }
@@ -132,12 +159,76 @@ mcuf_newdata(void)
     /* init writing of output-buffer to uart */
     mcuf_serial_senddata(); 
   } else
+
+
   /* EBLP Magic bytes - see https://wiki.blinkenarea.org/index.php/ExtendedBlinkenlightsProtocol */
   if ( strncmp(uip_appdata, "\xfe\xed\xbe\xef", 4) == 0) {
     /* input */
     struct eblp_packet *pkt = (struct eblp_packet *)uip_appdata;
     uint16_t height = htons(pkt->height);
     uint16_t width = htons(pkt->width);
+
+#ifdef MCUF_SERIAL_WORKAROUND_FOR_BAD_MCUF_UDP_PACKETS
+    /* scan packet to determine maxvalue (workaround for some stupid programms that dont send eblp according to https://wiki.blinkenarea.org/index.php/ExtendedBlinkenlightsProtocol but use 1 as maxvaule instead of 255) */
+    uint16_t maxvalue = 1; //assume we do have a packet from such a stupid programm
+    uint8_t x, y;
+    for (y = 0; y < height; y++) {
+      for (x = 0; x < width; x++) {
+        if (pkt->data[x + (y * width)] > 1) {
+          maxvalue = 255; //everything is fine and as described in the doku - use 255 as maxvalue
+          //syslog_sendf("Notice: found value bigger 1 on x %d y %d value %d", x, y, pkt->data[x + (y * width)]);
+          goto workaround_break_label;
+        }
+      }
+    }
+    workaround_break_label:
+#ifdef SYSLOG_SUPPORT
+    if (maxvalue == 1) {
+      //syslog_send_P(PSTR("Notice: Using maxvalue-workaround!"));
+    }
+#endif
+#else
+    uint16_t maxvalue = 255;
+    uint8_t x, y;
+#endif
+    /* init output-buffer */
+    memset(buffer.data, 0, 12+144);
+
+    /* write frame-data to output-buffer */
+    if (maxvalue == 1) {
+      /* b/w */
+      for (y = 0; y < 8; y++) {
+        for (x = 0; x < 18; x++) {
+          if ((height > y) && (width > x)) {
+            if (pkt->data[x + (y * width)] > 0) {
+              buffer.data[12 + (x + (y * 18))] = 255;
+            }
+          }
+        }
+      }
+    } else {
+      /* grayscaled */
+      for (y = 0; y < 8; y++) {
+        for (x = 0; x < 18; x++) {
+          if ((height > y) && (width > x)) {
+            buffer.data[12 + (x + (y * 18))] = pkt->data[x + (y * width)];
+          }
+        }
+      }
+    }
+
+    /* init writing of output-buffer to uart */
+    mcuf_serial_senddata(); 
+  } else
+
+
+  /* BLP Magic bytes - see https://wiki.blinkenarea.org/index.php/BlinkenlightsProtocol */
+  if ( strncmp(uip_appdata, "\xde\xad\xbe\xef", 4) == 0) {
+    /* input */
+    struct blp_packet *pkt = (struct blp_packet *)uip_appdata;
+    uint16_t height = htons(pkt->height);
+    uint16_t width = htons(pkt->width);
+
     /* init output-buffer */
     memset(buffer.data, 0, 12+144);
 
@@ -145,8 +236,10 @@ mcuf_newdata(void)
     uint8_t x, y;
     for (y = 0; y < 8; y++) {
       for (x = 0; x < 18; x++) {
-        if (height > y && width > x) {
-          buffer.data[12 + (x + (y * 18))] = pkt->data[x + (y * width)];
+        if ((height > y) && (width > x)) {
+          if (pkt->data[x + (y * width)] > 0) {
+            buffer.data[12 + (x + (y * 18))] = 255;
+          }
         }
       }
     }
