d6dc6c56a3ff97fe6ab7a39e23243639df92a22f
Christian Dietrich <stettberger@dokucode.de>
2008-09-27 22:38:21 +0000

    contrib: added webscripts

---
+++ contrib/webscripts/sty.c
@@ -0,0 +1,26 @@
+
+#logconsole {
+  padding: 5px 5px 5px 5px;
+  margin-top: 10px;
+  width: 98%;
+  border: medium dotted black;
+  visibility: hidden;
+}
+
+#logconsole .lognotice {
+  color: green;
+}
+
+#logconsole .logerror {
+  color: red;
+  font-weight: bold;
+}
+
+.iotable {
+  text-align: center;
+  padding: 15px; 
+  empty-cells: show;
+}
+
+.iotable td { border-bottom: 1px dashed #FF9999 }
+.iotable th { border-bottom: 2px dashed #9999FF; padding: 3px;}
