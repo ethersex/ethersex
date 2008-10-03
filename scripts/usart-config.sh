7a538fd3c23696b353c6d5ffb41a2836650de1f1
Stefan Siegl <stesie@brokenpipe.de>
2008-10-03 17:04:45 +0200

    menuconfig: restore usart configuration.

--- scripts/usart-config.sh
+++ scripts/usart-config.sh
@@ -1,6 +1,7 @@
 
-USARTS=$(( $(echo "#include <avr/io.h>" | avr-gcc -mmcu=$MCU -C -E -dD - | sed -n 's/.* UDR\([0-9]\+\).*/\1/p' | sort -n -r | sed -n 1p) + 1 ))
-USARTS_USED=0
+get_usart_count() {
+  USARTS=$(( $(echo "#include <avr/io.h>" | avr-gcc -mmcu=$MCU -C -E -dD - | sed -n 's/.* UDR\([0-9]\+\).*/\1/p' | sort -n -r | sed -n 1p) + 1 ))
+}
 
 usart_choice() {
   i=0
@@ -9,6 +10,7 @@ usart_choice() {
     i=$(( $i + 1))
   done
 }
+
 usart_process_choice() {
   i=0
   while [ $i -lt $USARTS ]; do
@@ -21,18 +23,25 @@ usart_process_choice() {
   done
 }
 
-if [ "$MODBUS_SUPPORT" = y ]; then
-  USARTS_USED=$(($USARTS_USED + 1))
-fi
-if [ "$YPORT_SUPPORT" = y ]; then
-  USARTS_USED=$(($USARTS_USED + 1))
-fi
-if [ "$ZBUS_SUPPORT" = y ]; then
-  USARTS_USED=$(($USARTS_USED + 1))
-fi
-if [ "$MCUF_SUPPORT" = y ]; then
-  USARTS_USED=$(($USARTS_USED + 1))
-fi
-if [ "$ECMD_SERIAL_USART_SUPPORT" = y ]; then
-  USARTS_USED=$(($USARTS_USED + 1))
-fi
+usart_count_used() {
+  USARTS_USED=0
+
+  if [ "$MODBUS_SUPPORT" = y ]; then
+    USARTS_USED=$(($USARTS_USED + 1))
+  fi
+  if [ "$YPORT_SUPPORT" = y ]; then
+    USARTS_USED=$(($USARTS_USED + 1))
+  fi
+  if [ "$ZBUS_SUPPORT" = y ]; then
+    USARTS_USED=$(($USARTS_USED + 1))
+  fi
+  if [ "$MCUF_SUPPORT" = y ]; then
+    USARTS_USED=$(($USARTS_USED + 1))
+  fi
+  if [ "$ECMD_SERIAL_USART_SUPPORT" = y ]; then
+    USARTS_USED=$(($USARTS_USED + 1))
+  fi
+}
+
+get_usart_count
+usart_count_used
