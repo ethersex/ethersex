c891793d5b1c825af98c7c8b3693189b3301ed18
Stefan Siegl <stesie@brokenpipe.de>
2008-10-03 14:19:34 +0200

    menuconfig: improve/rework whole system.
    Old profile mechanism has been dropped, in favour of templates
    that can be loaded at first.  Allow to enable any combination of
    teensy/router/interfaces, i.e. allow to build a firmware for
    ATmega8 with routing support between USB and RFM12 (teensy enabled
    of course).
    
    config.in has been split up into several (smaller) files located
    in the according subdirectory.

---
+++ scripts/usart-config.sh
@@ -0,0 +1,38 @@
+
+USARTS=$(( $(echo "#include <avr/io.h>" | avr-gcc -mmcu=$MCU -C -E -dD - | sed -n 's/.* UDR\([0-9]\+\).*/\1/p' | sort -n -r | sed -n 1p) + 1 ))
+USARTS_USED=0
+
+usart_choice() {
+  i=0
+  while [ $i -lt $USARTS ]; do
+    echo "$i  $1_USART_$i"
+    i=$(( $i + 1))
+  done
+}
+usart_process_choice() {
+  i=0
+  while [ $i -lt $USARTS ]; do
+  	this_usart=$(eval "echo \$${1}_USART_${i}")
+    if [ "$this_usart" = y ]; then
+      define_symbol "$1_USE_USART" $i
+      break
+    fi
+    i=$(( $i + 1))
+  done
+}
+
+if [ "$MODBUS_SUPPORT" = y ]; then
+  USARTS_USED=$(($USARTS_USED + 1))
+fi
+if [ "$YPORT_SUPPORT" = y ]; then
+  USARTS_USED=$(($USARTS_USED + 1))
+fi
+if [ "$ZBUS_SUPPORT" = y ]; then
+  USARTS_USED=$(($USARTS_USED + 1))
+fi
+if [ "$MCUF_SUPPORT" = y ]; then
+  USARTS_USED=$(($USARTS_USED + 1))
+fi
+if [ "$ECMD_SERIAL_USART_SUPPORT" = y ]; then
+  USARTS_USED=$(($USARTS_USED + 1))
+fi
