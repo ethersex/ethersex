3fe22446a05d858e5b6153197855aa005406a233
Stefan Siegl <stesie@brokenpipe.de>
2008-11-08 04:35:13 +0100

    httpd/embed: add io control cruft.

--- httpd/embed/sty.c
+++ httpd/embed/sty.c
@@ -5,3 +5,28 @@ body {
   font-family: sans-serif; 
 }
 
+#logconsole {
+  padding: 5px 5px 5px 5px;
+  margin-top: 10px;
+  width: 98%;
+  border: medium dotted black;
+  visibility: hidden;
+}
+
+#logconsole .lognotice {
+  color: green;
+}
+
+#logconsole .logerror {
+  color: red;
+  font-weight: bold;
+}
+
+.iotable {
+  text-align: center;
+  padding: 15px; 
+  empty-cells: show;
+}
+
+.iotable td { border-bottom: 1px dashed #FF9999 }
+.iotable th { border-bottom: 2px dashed #9999FF; padding: 3px;}
