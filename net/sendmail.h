fca4d26b180ff3052e0d4172b94faa01af9fcfda
Stefan Siegl <stesie@brokenpipe.de>
2008-11-18 17:23:56 +0100

    sendmail: convert to unix line-breaks, re-indent.

--- net/sendmail.h
+++ net/sendmail.h
@@ -21,51 +21,47 @@
  Falls nicht, schreiben Sie an die Free Software Foundation, 
  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA. 
 ------------------------------------------------------------------------------*/
+
 #include <config.h>
 
 #if USE_MAIL
 #ifndef _SENDMAIL_H
-	#define _SENDMAIL_H
+#define _SENDMAIL_H
 
-	#include <avr/io.h>
-    #include <string.h>
-	#include "usart.h"
-    #include "base64.h"
+#include <avr/io.h>
+#include <string.h>
+#include "usart.h"
+#include "base64.h"
+unsigned char mail_enable;
+unsigned int mail_send_counter;
+unsigned int my_mail_cp;	//cp = CLIENT_PORT
 
-    unsigned char mail_enable;
-    unsigned int mail_send_counter;
-    unsigned int my_mail_cp; //cp = CLIENT_PORT
-    
-    //Port des EMAIL-SERVERS
-    #define MAIL_PORT 25
-    
-    //Mit oder ohne Debug-Ausgabe
-    #define MAIL_DEBUG usart_write	//mit Debugausgabe
-    //#define MAIL_DEBUG(...) 			//ohne Debugausgabe
+  //Port des EMAIL-SERVERS
+#define MAIL_PORT 25
 
-    //IP des MAIL-Servers 
-    #define MAIL_SERVER	IP(212,227,15,183) //hier z.B. smtp.1und1.de
-    //#define MAIL_SERVER	IP(69,147,102,58) //hier z.B. smtp.mail.yahoo.com
-    //#define MAIL_SERVER	IP(213,165,64,20) //hier z.B. mail.gmx.net
-    
-    //Hier Username eintragen
-    #define SMTP_USERNAME "tester\0"
-    
-    //Hier das Passwort eintragen
-    #define SMTP_PASSWORD "tester\0"
-    
-    //SMTP_MAIL Sender
-    #define __SMTP_MAIL_FROM__ "mail@ulrichradig.de" 
-    
-    //SMTP_MAIL Empfänger
-    #define __SMTP_MAIL_TO__ "mail@ulrichradig.de"
-    
-    unsigned char mail_send (void);
-    void mail_client_init (void);
-	void mail_data (unsigned char);
+  //Mit oder ohne Debug-Ausgabe
+#define MAIL_DEBUG usart_write	//mit Debugausgabe
+  //#define MAIL_DEBUG(...)                   //ohne Debugausgabe
 
+  //IP des MAIL-Servers 
+#define MAIL_SERVER	IP(212,227,15,183)	//hier z.B. smtp.1und1.de
+  //#define MAIL_SERVER       IP(69,147,102,58) //hier z.B. smtp.mail.yahoo.com
+  //#define MAIL_SERVER       IP(213,165,64,20) //hier z.B. mail.gmx.net
+
+  //Hier Username eintragen
+#define SMTP_USERNAME "tester\0"
+
+  //Hier das Passwort eintragen
+#define SMTP_PASSWORD "tester\0"
+
+  //SMTP_MAIL Sender
+#define __SMTP_MAIL_FROM__ "mail@ulrichradig.de"
+
+  //SMTP_MAIL Empfänger
+#define __SMTP_MAIL_TO__ "mail@ulrichradig.de"
+unsigned char mail_send (void);
+void mail_client_init (void);
+void mail_data (unsigned char);
 
 #endif //_SENDMAIL_H
 #endif //USE_MAIL
-
-
