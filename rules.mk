ed5fb3b0019164544d2d8a3deb8271df09583531
Stefan Siegl <stesie@brokenpipe.de>
2008-12-21 23:33:58 +0100

    rules.mk: automatically generate pinning.c file
    ... if needed in sub-directory and not existing.

--- rules.mk
+++ rules.mk
@@ -1,3 +1,11 @@
+#
+# rules.mk
+#
+# Make rules common to all sub-directories of the build system.
+# This file must NOT be sourced from the Makefile in ${TOPDIR}.
+# Rules that should be shared by the TOPDIR-Makefile even also
+# should go into ${TOPDIR}/defaults.mk
+#
 
 OBJECTS += $(patsubst %.c,%.o,${SRC})
 OBJECTS += $(patsubst %.S,%.o,${ASRC})
@@ -17,5 +25,8 @@ clean: clean-common
 
 all:
 	make -C $(TOPDIR) all
+
+$(TOPDIR)/pinning.c:
+	make -C $(TOPDIR) pinning.c
 
 include $(TOPDIR)/depend.mk
