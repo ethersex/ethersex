37766a8069cd08974267917efa666ee2e77f77d7
Christian Dietrich <stettberger@dokucode.de>
2009-03-08 17:20:12 +0100

    vfs/embed: named pin page may be disabled

--- vfs/embed/np.ht.sh
+++ vfs/embed/np.ht.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if grep -q 'NAMED_PIN_SUPPORT=y' .config; then
+if grep -q 'NAMED_PIN_INLINE_SUPPORT=y' .config; then
   bash named_pin/np_web.sh named_pin/config  | m4 named_pin/np_web.m4 -
 else
   exit 1;
