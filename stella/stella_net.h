68edb30854779b055e657f4aad05511171479a22
David Gräff <david.graeff@web.de>
2009-04-04 17:52:54 +0200

    Stella: Change license to GPLv2 or later
    Signed-off-by: Stefan Siegl <stesie@brokenpipe.de>

--- stella/stella_net.h
+++ stella/stella_net.h
@@ -2,9 +2,10 @@
  * Copyright (c) 2007 by Stefan Siegl <stesie@brokenpipe.de>
  * Copyright (c) 2009 by David Gräff <david.graeff@web.de>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
--- stella/stella_pwm.c
+++ stella/stella_pwm.c
@@ -1,22 +1,27 @@
 /*
- Copyright(C) 2006 Christian Dietrich <stettberger@dokucode.de>
- Copyright(C) 2006 Jochen Roessner <jochen@lugrot.de>
- Copyright(C) 2007 Stefan Siegl <stesie@brokenpipe.de>
- Copyright(C) 2008 Jochen Roessner <jochen@lugrot.de>
- Copyright(C) 2009 David Gräff <david.graeff@web.de>
-
- This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+ * Copyright(C) 2006 Christian Dietrich <stettberger@dokucode.de>
+ * Copyright(C) 2006 Jochen Roessner <jochen@lugrot.de>
+ * Copyright(C) 2007 Stefan Siegl <stesie@brokenpipe.de>
+ * Copyright(C) 2008 Jochen Roessner <jochen@lugrot.de>
+ * Copyright(C) 2009 David Gräff <david.graeff@web.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * For more information on the GPL, please go to:
+ * http://www.gnu.org/copyleft/gpl.html
+ */
 
 #include <avr/interrupt.h>
 #include <avr/io.h>
