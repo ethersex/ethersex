08481a4d9d12b31e17c0a79c2caae53ffc63ffe8
Stefan Siegl <stesie@brokenpipe.de>
2009-04-12 17:34:45 +0200

    pwm_wav.c: relicense to GPLv2+
    According to Message-Id: <1239537520.4165.36.camel@localhost>

--- hardware/pwm/pwm_wav.c
+++ hardware/pwm/pwm_wav.c
@@ -2,12 +2,12 @@
  *
  * Copyright (c) 2009 by Christian Dietrich <stettberger@dokucode.de>
  * Copyright (c) 2009 by Stefan Riepenhausen <rhn@gmx.net>
- * Copyright (c) 2008 by Markus Meissna <markus@meissna.de>
  * Copyright (c) by Ulrich Radig <mail@ulrichradig.de>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
