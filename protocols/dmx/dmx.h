b28774439ff62c092158f299a89a4656d4b079bc
Dirk Pannenbecker <veyron@zerties.org>
2009-06-21 20:40:47 +0200

    dmx: added dmx output

---
+++ protocols/dmx/dmx.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2009 by Dirk Pannenbecker <dp@sd-gp.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * For more information on the GPL, please go to:
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+#include "config.h"
+
+#ifndef _DMX_H
+#define _DMX_H
+
+#ifdef DMX_SUPPORT
+void dmx_init(void);
+void dmx_periodic(void);
+void dmx_set_chan_x(uint8_t startchan, uint8_t channum, uint8_t *chan);
+
+extern unsigned char dmx_data[CONF_DMX_MAX_CHAN];
+extern volatile uint8_t dmx_index;
+extern volatile uint8_t dmx_txlen;
+extern volatile uint8_t dmx_prg;
+#endif DMX_SUPPORT
+#endif _DMX_H
