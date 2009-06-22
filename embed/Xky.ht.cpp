34eb1ad64dd0d91ae34bd78d204c45463a2adb20
Peter Marschall <peter@adpm.de>
2009-06-22 08:16:49 +0200

    adapt Xky.ht to Xow.ht style, use new gph.js methods
    Signed-off-by: Peter Marschall <peter@adpm.de>
    Signed-off-by: Stefan Siegl <stesie@brokenpipe.de>

--- embed/Xky.ht.cpp
+++ embed/Xky.ht.cpp
@@ -17,60 +17,63 @@
     <script type="text/javascript"><![CDATA[
 var num = 8;
 var min = -10, max = 50;
-var g = new Array();
-
-function kty_trigger() {
-	setTimeout("kty_trigger_get();", 10000);
-}
+var g;
 
 function kty_trigger_get() {
-	ArrAjax.ecmd('kty get', kty_get_handler, 'GET', 0);
-	kty_trigger();
+	setInterval("ArrAjax.ecmd('kty get', kty_get_handler)", 5000);
 }
 
 function kty_get_handler(request, data) {
-	var daten = returnObjById("daten");
-	daten.innerHTML = request.responseText;
-
+	var sensors = request.responseText.split(" ");
+	for (var i = sensors.length-1; i >= 0; i--) {
+		if (isNaN(parseFloat(sensors[i])))
+			sensors.splice(i, 1);
+	}
+	num = (sensors.length < num) ? sensors.length : num;
 	for (var i = 0; i < num; i++) {
-		var sensor = request.responseText.substr(i*6, 6);
-		graphAppend(g[i], parseInt(sensor));
+		var dat = $('kty_data' + i);
+		dat.innerHTML = sensors[i] + '°C';
+		dat.style.color = g[i].color;
+		g[i].append(parseInt(sensors[i]));
 	}
 }
 
 window.onload = function() {
-	graphCreateAxis("axis", "text", min, max);
+	g = initDiagram(num, "grph#", "axis", "text", min, max);
+	var kty_table = $('kty_table');
 	for (var i = 0; i < num; i++)
-		g[i] = new Graph("grph" + i, 40, min, max);
-	kty_trigger();
+		kty_table.insertRow(i+1).innerHTML = '<td id="kty_data' + i +'">No data</td>';
+	kty_trigger_get();
 }
 ]]></script>
   </head>
   <body>
     <h1>SVG-powered KTY Status</h1>
 
-    <p><!-- Jippie, we like Microsoft Internet Explorer -->
-      <object id="AdobeSVG" classid="clsid:78156a80-c6a1-4bbf-8e6a-3cd390eeb4e2"> </object>
-    </p>
-
-    <?import namespace="svg" urn="http://www.w3.org/2000/svg" implementation="#AdobeSVG"?>
-    <svg:svg width="400px" height="300px" viewBox="0 0 400 300"
-	     zoomAndPan="disable">
-      <svg:g stroke="red"   style="stroke-width:2px;" id="grph0"></svg:g>
-      <svg:g stroke="blue"  style="stroke-width:2px;" id="grph1"></svg:g>
-      <svg:g stroke="green" style="stroke-width:2px;" id="grph2"></svg:g>
-      <svg:g stroke="lime" style="stroke-width:2px;" id="grph3"></svg:g>
-      <svg:g stroke="purple" style="stroke-width:2px;" id="grph4"></svg:g>
-      <svg:g stroke="maroon" style="stroke-width:2px;" id="grph5"></svg:g>
-      <svg:g stroke="navy" style="stroke-width:2px;" id="grph6"></svg:g>
-      <svg:g stroke="yellow" style="stroke-width:2px;" id="grph7"></svg:g>
-      <svg:g stroke="#999"  style="stroke-dasharray: 2, 5; " id="axis"></svg:g>
-      <svg:g stroke="#999" font-size="12" id="text"></svg:g>
-    </svg:svg>
-
-    <table id='ow_table' border="1" cellspacing="0">
-    <tr><td>Daten</td></tr>
-    <tr><td id='daten'>No data</td></tr>
+    <table>
+    <tr><td valign="top">
+      <table id='kty_table' border="1" cellspacing="0">
+        <tr><td>Data</td></tr>
+      </table>
+    </td>
+    <td>
+      <p><!-- Jippie, we like Microsoft Internet Explorer -->
+        <object id="AdobeSVG" classid="clsid:78156a80-c6a1-4bbf-8e6a-3cd390eeb4e2"> </object>
+      </p>
+      <?import namespace="svg" urn="http://www.w3.org/2000/svg" implementation="#AdobeSVG"?>
+      <svg:svg width="400px" height="300px" viewBox="0 0 400 300" zoomAndPan="disable">
+        <svg:g stroke="red"   style="stroke-width:2px;" id="grph0"></svg:g>
+        <svg:g stroke="blue"  style="stroke-width:2px;" id="grph1"></svg:g>
+        <svg:g stroke="green" style="stroke-width:2px;" id="grph2"></svg:g>
+        <svg:g stroke="lime"  style="stroke-width:2px;" id="grph3"></svg:g>
+        <svg:g stroke="purple" style="stroke-width:2px;" id="grph4"></svg:g>
+        <svg:g stroke="maroon" style="stroke-width:2px;" id="grph5"></svg:g>
+        <svg:g stroke="navy"   style="stroke-width:2px;" id="grph6"></svg:g>
+        <svg:g stroke="yellow" style="stroke-width:2px;" id="grph7"></svg:g>
+        <svg:g stroke="#999"  style="stroke-dasharray: 2, 5; " id="axis"></svg:g>
+        <svg:g stroke="#999" font-size="12" id="text"></svg:g>
+      </svg:svg>
+    </td></tr>
     </table>
     <div id="logconsole"></div>
   </body>
