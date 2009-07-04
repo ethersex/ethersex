0e8662778c8950f1933bd9ecbbd66dcafac0f11e
Stefan Siegl <stesie@brokenpipe.de>
2009-07-04 13:12:16 +0200

    depend.mk: generate .dep files instead of .d files to not name-clash on lang.d

--- scripts/depend.mk
+++ scripts/depend.mk
@@ -9,8 +9,8 @@ ifneq ($(MAKECMDGOALS),clean)
 ifneq ($(MAKECMDGOALS),mrproper)
 ifneq ($(MAKECMDGOALS),menuconfig)
 
-# For each .o file we need a .d file.
-sinclude $(foreach file,$(subst .o,.d,$(filter %.o,$(OBJECTS))),$(TOPDIR)/${file})
+# For each .o file we need a .dep file.
+sinclude $(foreach file,$(subst .o,.dep,$(filter %.o,$(OBJECTS))),$(TOPDIR)/${file})
 
 endif
 endif
@@ -25,9 +25,9 @@ $(CC) $(CFLAGS) $(CPPFLAGS) -M -MM $< | sed > $@.new -e 's;$(*F)\.o:;$@ $*.o $*.
 if test -s $@.new; then mv -f $@.new $@; else rm -f $@.new; fi
 endef
 
-# Here is how to make .d files from .c files
-%.d: %.c $(TOPDIR)/control6/control6.h $(TOPDIR)/pinning.c
+# Here is how to make .dep files from .c files
+%.dep: %.c $(TOPDIR)/control6/control6.h $(TOPDIR)/pinning.c
 	@$(make-deps)
 
-%.d: %.S
+%.dep: %.S
 	@$(make-deps)
