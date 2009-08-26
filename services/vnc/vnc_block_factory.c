1246d06c25c2f3f84d5db6947a96b41862828bc9
Christian Dietrich <stettberger@dokucode.de>
2009-08-26 14:48:00 +0200

    vnc: add vnc_block_factory to enable updates

---
+++ services/vnc/vnc_block_factory.c
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2009 by Christian Dietrich <stettberger@dokucode.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * For more information on the GPL, please go to:
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+#include <string.h>
+#include "protocols/uip/uip.h"
+#include "core/debug.h"
+#include "vnc.h"
+#include "vnc_state.h"
+#include "vnc_block_factory.h"
+
+#include "config.h"
+
+void
+vnc_make_block(struct vnc_block *dest, uint8_t x, uint8_t y)
+{
+    dest->x = HTONS(x * VNC_BLOCK_WIDTH);
+    dest->y = HTONS(y * VNC_BLOCK_HEIGHT);
+    dest->w = HTONS(VNC_BLOCK_WIDTH);
+    dest->h = HTONS(VNC_BLOCK_HEIGHT);
+    dest->encoding = 0;
+    
+    memset(dest->data, x % 2 ? 0xff : 0, VNC_BLOCK_LENGTH);
+}
---
+++ services/vnc/vnc_block_factory.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2009 by Christian Dietrich <stettberger@dokucode.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * For more information on the GPL, please go to:
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+#ifndef _VNC_BLOCK_FACTORY
+#define _VNC_BLOCK_FACTORY
+
+#include <inttypes.h>
+
+/* The screensize is fixed to 255x255 pixel FIXME */
+#define VNC_BLOCK_HEIGHT 16
+#define VNC_BLOCK_WIDTH  16
+#define VNC_BLOCK_ROWS   16
+#define VNC_BLOCK_COLS   16 /* Maximum is 16 FIXME */
+#define VNC_BLOCK_LENGTH (VNC_BLOCK_WIDTH * VNC_BLOCK_HEIGHT)
+
+#define VNC_SCREEN_WIDTH (VNC_BLOCK_WIDTH * VNC_BLOCK_COLS)
+#define VNC_SCREEN_HEIGHT (VNC_BLOCK_HEIGH * VNC_BLOCK_ROWS)
+
+struct vnc_block {
+  uint16_t x;
+  uint16_t y;
+  uint16_t w;
+  uint16_t h;
+  uint32_t encoding;
+  uint8_t data[VNC_BLOCK_LENGTH];
+};
+
+struct vnc_update_header {
+  uint8_t type; /* message type, update == 0 */
+  uint8_t padding; /* No Idea what this means */
+  uint16_t block_count;
+  struct vnc_block blocks[];
+};
+
+
+/* x and y are block addresses */
+void vnc_make_block(struct vnc_block *dest, uint8_t x, uint8_t y); 
+
+#endif /* _VNC_BLOCK_FACTORY */
