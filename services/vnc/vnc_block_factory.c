bac00b7eff6b582faa1914f90705aa974ee8abb7
Christian Dietrich <stettberger@dokucode.de>
2009-09-05 11:52:45 +0200

    vnc: add vnc_putchar to print an character

--- services/vnc/vnc_block_factory.c
+++ services/vnc/vnc_block_factory.c
@@ -29,12 +29,87 @@
 #include "vnc.h"
 #include "vnc_state.h"
 #include "vnc_block_factory.h"
+#include "vnc_font.h"
 
 #include "config.h"
+
+void 
+vnc_putchar(struct vnc_block *dest,
+            char data, 
+            uint8_t color, 
+            uint8_t char_line, 
+            uint8_t char_column) 
+{
+    uint8_t x, y;
+    /* We have to select the right line */
+    if (char_line / 2 != dest->y) return;
+
+/*
+  ______
+  ______
+  _XX___
+  ___X__
+  _XXX__
+  X__X__
+  _XXX__
+  ______ 
+   ^  ^
+   |  + char_x_offset + char_x_len;
+   +- char_x_offset
+ */
+    /* Now we select the right row */
+    uint16_t tmp = char_column * 6;
+    if (! (tmp / VNC_BLOCK_WIDTH == dest->x)  
+        && ! ((tmp / VNC_BLOCK_WIDTH == (dest->x - 1) && tmp > (dest->x * VNC_BLOCK_WIDTH - 6)))) return;
+
+    uint8_t char_x_offset, char_x_len;
+
+    /* Offset within the block */
+    uint8_t x_offset;
+
+    /* Start Pixel of character in block */
+    if (tmp >= dest->x * VNC_BLOCK_WIDTH) {
+        char_x_offset = 0;
+        x_offset = tmp % VNC_BLOCK_WIDTH;
+        if (tmp + 6 > (dest->x + 1) * VNC_BLOCK_WIDTH)
+            char_x_len =  (dest->x + 1) * VNC_BLOCK_WIDTH - tmp;
+        else
+            char_x_len = 6;
+    } else {
+        /* Start Pixel is in the block before */
+        char_x_offset = dest->x * VNC_BLOCK_WIDTH - tmp;
+        char_x_len = 6 - char_x_offset;
+        x_offset = 0;
+    } 
+        
+
+    for (x = 0; x <  char_x_len; x++) {
+        for (y = 0; y < 8; y++) {
+            if (vnc_font[(uint8_t)data][char_x_offset + x] & _BV(y)) {
+                dest->data[((char_line % 2) * 8 + y) * VNC_BLOCK_WIDTH + x + x_offset ] = color;
+            }
+        }
+    }
+}
 
 void
 vnc_make_block(struct vnc_block *dest, uint8_t block_x, uint8_t block_y)
 {
+    /* This is only for the helper functions */
+    dest->x = block_x;
+    dest->y = block_y;
+	
+    uint8_t x, y;
+    memset(dest->data, 0xff, sizeof(dest->data));
+
+
+    char data[] = "HALLO WELT!!!";
+    uint8_t len = strlen("HALLO WELT!!!");
+    for (x = 0; x < len; x++)
+        vnc_putchar(dest, data[x], 0, 1, x);
+    for (x = 0; x < len; x++)
+        vnc_putchar(dest, data[x], 0, 0, x);
+
     dest->x = HTONS(block_x * VNC_BLOCK_WIDTH);
     dest->y = HTONS(block_y * VNC_BLOCK_HEIGHT);
     dest->w = HTONS(VNC_BLOCK_WIDTH);
@@ -42,14 +117,4 @@ vnc_make_block(struct vnc_block *dest, uint8_t block_x, uint8_t block_y)
     dest->encoding = 0;
 
     
-    uint8_t _x, _y;
-    uint16_t x, y;
-    for (_x = 0; _x < VNC_BLOCK_WIDTH; _x++)
-      for (_y = 0; _y < VNC_BLOCK_HEIGHT; _y++)  {
-        x = block_x * VNC_BLOCK_WIDTH + _x;
-        y = block_y * VNC_BLOCK_HEIGHT + _y;
-
-        dest->data[_y * VNC_BLOCK_WIDTH + _x] = x + y;
-
-      }
 }
