398c7014c86b7b7b0ad917579534e8dadf13815a
Stefan Siegl <stesie@brokenpipe.de>
2009-09-05 23:17:21 +0200

    host: make %S format arg in printf work.

--- core/host/avr/pgmspace.h
+++ core/host/avr/pgmspace.h
@@ -1,19 +1,23 @@
 #ifndef AVR_PGMSPACE_H
 #define AVR_PGMSPACE_H
 
-#define sscanf_P(a...)		sscanf(a)
+#include <string.h>
 
-#define printf_P(a...)		printf(a)
-#define sprintf_P(a...)		sprintf(a)
-#define snprintf_P(a...)	snprintf(a)
+int printf_P (const char *fmt, ...);
+int sprintf_P (char *, const char *fmt, ...);
+int snprintf_P (char *, int, const char *fmt, ...);
+
+#define sscanf_P(a...)		sscanf(a)
 
 #define memcmp_P(a...)		memcmp(a)
 #define memcpy_P(a...)		memcpy(a)
 
 #define strlen_P(a...)		strlen(a)
 #define strstr_P(a...)		strstr(a)
+#define strcat_P(a...)		strcat(a)
 #define strcpy_P(a...)		strcpy(a)
 #define strncmp_P(a...)		strncmp(a)
+#define strncasecmp_P(a...)	strncasecmp(a)
 
 #define pgm_read_word(a)	(*(a))
 #define pgm_read_byte(a)	(*(a))
