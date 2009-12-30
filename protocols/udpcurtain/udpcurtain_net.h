7ddc9c0cd45a81e5809902b38fcdb484d859e529
David Gräff <david@davidPC.(none)>
2009-12-30 23:34:16 +0100

    Add a simple udp control protocol for the motor curtain module

---
+++ protocols/udpcurtain/udpcurtain_net.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2009 by David Gräff
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * For more information on the GPL, please go to:
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+#ifndef UDPCURTAIN_NET_H
+#define UDPCURTAIN_NET_H
+
+void udpcurtain_net_init(void);
+void udpcurtain_net_main(void);
+
+#endif /* UDPCURTAIN_NET_H */
