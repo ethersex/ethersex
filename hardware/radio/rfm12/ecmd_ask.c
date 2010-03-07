19b3a360ccd9e69c5ae76c820a6ea829b687b7f2
Stefan Riepenhausen <rhn@gmx.net>
2010-03-07 22:16:45 +0100

    RFM ASK: remove test code

--- hardware/radio/rfm12/ecmd_ask.c
+++ hardware/radio/rfm12/ecmd_ask.c
@@ -177,21 +177,10 @@ parse_cmd_rfm12_ask_sense (char *cmd, char *output, uint16_t len)
 #endif  /* RFM12_ASK_SENSING_SUPPORT */
 #endif  /* RFM12_ASK_EXTERNAL_FILTER_SUPPORT */
 
-int16_t
-parse_cmd_rfm12_ask_test (char *cmd, char *output, uint16_t len)
-{
-   uint8_t val=0;
-	uint8_t ret = sscanf_P(cmd, PSTR("%hhu"), &val);
-	if (ret == 0)
-		return ECMD_FINAL_OK;
-	else
-      return ECMD_FINAL(snprintf_P(output, len, PSTR("%u"), val));
-}
 /*
 -- Ethersex META --
   block(RFM ASK)
   ecmd_ifdef(RFM12_ASK_INTERTECHNO_SUPPORT)
-    ecmd_feature(rfm12_ask_test, "rfm12 test", [1], val)
     ecmd_feature(rfm12_ask_intertechno_send, "rfm12 intertechno", , family group device command)
   ecmd_endif()
 */
