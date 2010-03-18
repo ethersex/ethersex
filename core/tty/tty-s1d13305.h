a2ff4cdc8b54214acbd7b0166deae3afe4396687
FordPrfkt <fordprfkt@googlemail.com>
2010-03-18 21:56:55 +0100

    Kommentar korrigiert

--- core/tty/tty-s1d13305.h
+++ core/tty/tty-s1d13305.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009 by Stefan Siegl <stesie@brokenpipe.de>
+ * Copyright (c) 2009 by Daniel Walter <fordprfkt@googlemail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
