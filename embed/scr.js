ddf3a14d88223878bd282d9f092778b422fe8869
Peter Marschall <peter@adpm.de>
2010-05-08 18:36:42 +0200

    scr.js: save a few bytes by harmonizing
    Harmonizations:
    - string 'No Ajax support' used in both alert() and Exception
    - consistently use single quote for string constants
    - method call harmonization: "new XXX()" instead of "new XXX"
    
    Signed-off-by: Peter Marschall <peter@adpm.de>
    Signed-off-by: Christian Dietrich <stettberger@peer.zerties.org>

--- embed/scr.js
+++ embed/scr.js
@@ -21,7 +21,7 @@ var ArrAjax = new Object();
 ArrAjax.aufruf = function (address, handler, method, data) {
 	if (AjaxQueueLock.locked) {
 		// if (AjaxQueue.length > 20) return;
-		var request = new Object;
+		var request = new Object();
 		request.address = address;
 		request.handler = handler;
 		request.method = method;
@@ -34,17 +34,17 @@ ArrAjax.aufruf = function (address, handler, method, data) {
 
 	var xmlHttp = null;
 	if (!method)
-		 method = "GET";
+		 method = 'GET';
 	// Mozilla, Opera, Safari sowie Internet Explorer 7
 	if (typeof XMLHttpRequest != 'undefined')
 		xmlHttp = new XMLHttpRequest();
   	if (!xmlHttp) {
 		// Internet Explorer 6 und älter
 		try {
-			xmlHttp = new ActiveXObject("Msxml2.XMLHTTP");
+			xmlHttp = new ActiveXObject('Msxml2.XMLHTTP');
 		} catch(e) {
 			try {
-				xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+				xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');
 			} catch(e) {
 				xmlHttp = null;
 			}
@@ -52,8 +52,8 @@ ArrAjax.aufruf = function (address, handler, method, data) {
 	}
 
 	if (!xmlHttp) {
-		alert('No Ajax support possible');
-		throw Exception("No Ajax support");
+		alert('No Ajax support');
+		throw Exception('No Ajax support');
 		return;
 	}
 	xmlHttp.open(method, address, true);
@@ -99,24 +99,24 @@ function log_clean(bottom) {
 
 function logger(code, text, bottom) {
 	var logconsole = $('logconsole');
-	logconsole.style.visibility = "visible";
+	logconsole.style.visibility = 'visible';
 
 	var jetzt = new Date();
 	var Std = jetzt.getHours();
 	var Min = jetzt.getMinutes();
-	var Stdtext = (Std < 10) ? "0" + Std : Std;
-	var Mintext = (Min < 10) ? "0" + Min : Min;
-	var text = Stdtext + ":" + Mintext + " " + text;
+	var Stdtext = (Std < 10) ? '0' + Std : Std;
+	var Mintext = (Min < 10) ? '0' + Min : Min;
+	var text = Stdtext + ':' + Mintext + ' ' + text;
 
-	var neuText = document.createElement("div");
+	var neuText = document.createElement('div');
 	if (code == '0') {
-		neuText.className = "lognotice";
+		neuText.className = 'lognotice';
 		neuText.innerHTML = text;
 	} else if (code == 'notice') {
-		neuText.className = "logerror";
+		neuText.className = 'logerror';
 		neuText.innerHTML = text;
 	} else {
-		neuText.className = "logerror";
+		neuText.className = 'logerror';
 		neuText.innerHTML = 'Fehler: ' + text;
 	}
 	if (bottom)
@@ -129,6 +129,6 @@ function logger(code, text, bottom) {
 function ecmd_error(response) {
 	if (!response)
 		return true;
-        return (response.responseText.indexOf("parse error") != -1);
+        return (response.responseText.indexOf('parse error') != -1);
 }
 
