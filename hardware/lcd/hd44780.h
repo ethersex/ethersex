281898d313b3deea01b7157862fc6e05c0333041
Benjamin Velmeden <devel@velmeden.info>
2010-06-10 01:43:09 +0800

    Added aditional display, preparation for displays with 2 enable pins

--- hardware/lcd/hd44780.h
+++ hardware/lcd/hd44780.h
@@ -32,34 +32,98 @@
 #define HD44780_DISPTECH 2
 #define HD44780_KS0067B  3
 #define HD44780_KS0066U  4
+//#define HD44780_WDC2704  5
 
 #define HD44780_DIREKT 16
 #define HD44780_SERLCD 17
 #define HD44780_I2CSUPPORT 18
 
+
+/*Definition for different display types
+  Add some new Displays here*/
+#if HD44780_TYPE == HD44780_ORIGINAL
+    #define LCD_CHAR_PER_LINE 20 	
+    #define LCD_LINES 4 		
+    #define LCD_LINE_1_ADR 0x00
+    #define LCD_LINE_2_ADR 0x20
+    #define LCD_LINE_3_ADR 0x40
+    #define LCD_LINE_4_ADR 0x60
+    #define LCD_LINE_1_EN 1
+    #define LCD_LINE_2_EN 1
+    #define LCD_LINE_3_EN 1
+    #define LCD_LINE_4_EN 1
+#elif HD44780_TYPE == HD44780_DISPTECH
+    #define LCD_CHAR_PER_LINE 20 	
+    #define LCD_LINES 4 		
+    #define LCD_LINE_1_ADR 0x00
+    #define LCD_LINE_2_ADR 0x40
+    #define LCD_LINE_3_ADR 0x10
+    #define LCD_LINE_4_ADR 0x50
+    #define LCD_LINE_1_EN 1
+    #define LCD_LINE_2_EN 1
+    #define LCD_LINE_3_EN 1
+    #define LCD_LINE_4_EN 1
+#elif HD44780_TYPE == HD44780_KS0067B
+    #define LCD_CHAR_PER_LINE 20 	
+    #define LCD_LINES 4 		
+    #define LCD_LINE_1_ADR 0x00
+    #define LCD_LINE_2_ADR 0x40
+    #define LCD_LINE_3_ADR 0x14
+    #define LCD_LINE_4_ADR 0x54
+    #define LCD_LINE_1_EN 1
+    #define LCD_LINE_2_EN 1
+    #define LCD_LINE_3_EN 1
+    #define LCD_LINE_4_EN 1
+#elif HD44780_TYPE == HD44780_KS0066U
+    #define LCD_CHAR_PER_LINE 20 	
+    #define LCD_LINES 4 		
+    #define LCD_LINE_1_ADR 0x00
+    #define LCD_LINE_2_ADR 0x40
+    #define LCD_LINE_3_ADR 0x10
+    #define LCD_LINE_4_ADR 0x50
+    #define LCD_LINE_1_EN 1
+    #define LCD_LINE_2_EN 1
+    #define LCD_LINE_3_EN 1
+    #define LCD_LINE_4_EN 1
+#elif HD44780_TYPE == HD44780_WDC2704
+    #define LCD_CHAR_PER_LINE 27 	
+    #define LCD_LINES 4 		
+    #define LCD_LINE_1_ADR 0x00
+    #define LCD_LINE_2_ADR 0x40
+    #define LCD_LINE_3_ADR 0x00
+    #define LCD_LINE_4_ADR 0x40
+    #define LCD_LINE_1_EN 1
+    #define LCD_LINE_2_EN 1
+    #define LCD_LINE_3_EN 2
+    #define LCD_LINE_4_EN 2
+#else
+#error "unknown hd44780 compatible controller type!"
+
+#endif
+
 /* lcd stream */
 extern FILE *lcd;
 
-#define clock_write() clock_rw(0)
-#define clock_read() clock_rw(1)
+#define clock_write(en) clock_rw(0,en)
+#define clock_read(en) clock_rw(1,en)
 
 #define noinline __attribute__((noinline))
 
 /* prototypes */
 void hd44780_init(void);
-void hd44780_config(uint8_t cursor, uint8_t blink);
-void hd44780_define_char(uint8_t n_char, uint8_t *data);
+void hd44780_config(uint8_t cursor, uint8_t blink, uint8_t en);
+void hd44780_define_char(uint8_t n_char, uint8_t *data, uint8_t en);
 void hd44780_backlight(uint8_t state);
-void noinline hd44780_clear(void);
-void noinline hd44780_home(void);
+void noinline hd44780_clear();
+void noinline hd44780_home();
 void noinline hd44780_goto(uint8_t line, uint8_t pos);
-void noinline hd44780_shift(uint8_t right);
+void noinline hd44780_shift(uint8_t right, uint8_t en);
 int noinline hd44780_put(char d, FILE *stream);
 void noinline hd44780_hw_init(void);
-uint8_t noinline clock_rw(uint8_t read);
-void noinline output_nibble(uint8_t rs, uint8_t nibble);
+uint8_t noinline clock_rw(uint8_t read, uint8_t en);
+void noinline output_nibble(uint8_t rs, uint8_t nibble, uint8_t en );
 #ifdef HD44780_READBACK
-uint8_t noinline input_nibble(uint8_t rs);
+uint8_t noinline input_nibble(uint8_t rs, uint8_t en);
 #endif
 
 #endif /* HD44780_SUPPORT */
