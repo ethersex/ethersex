a01e712650561129cc00c13612439e46b7ba8b59
Maximilian Güntner <maximilian.guentner@gmail.com>
2011-04-03 23:01:37 +0200

    fixes avr reset due to a buffer overflow when calling channel without an argument (dumps all channels) while stella channels > 12 (output length of channel is 3+4n-1)

--- protocols/ecmd/via_tcp/ecmd_state.h
+++ protocols/ecmd/via_tcp/ecmd_state.h
@@ -23,7 +23,7 @@
 #define ECMD_STATE_H
 
 #define ECMD_INPUTBUF_LENGTH  50
-#define ECMD_OUTPUTBUF_LENGTH 50
+#define ECMD_OUTPUTBUF_LENGTH 258
 
 struct ecmd_connection_state_t {
     char inbuf[ECMD_INPUTBUF_LENGTH];
