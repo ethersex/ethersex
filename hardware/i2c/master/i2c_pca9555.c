6991d2fd2d9dba6f7a3b931eb2605bd6df05770f
FordPrfkt <fordprfkt@googlemail.com>
2011-12-31 03:12:10 +0100

    Commented

--- hardware/i2c/master/i2c_pca9555.c
+++ hardware/i2c/master/i2c_pca9555.c
@@ -20,6 +20,7 @@
  */
 
 #include <avr/io.h>
+#include <core/bit-macros.h>
 #include <util/twi.h>
 #include <stdbool.h>
 
@@ -30,50 +31,62 @@
 
 #ifdef I2C_PCA9555_SUPPORT
 
-#define LOW_BYTE(_v) (0xFF & _v)
-#define HIGH_BYTE(_v) ((0xFF00 & _v) >> 8)
-
-uint8_t i2c_pca9555_readWord(uint8_t addrOffset, uint8_t reg, uint16_t* value)
+/**
+* @brief Read one word (16 bit) from the chip.
+*
+* Addresses the chip and reads both bytes from the given register into value_pui16.
+* Note: The low byte of the desired register must be given in reg_ui8 or the bytes in
+* the result will be reversed.
+*
+* @param[in] addrOffset_ui8 Offset to the slave address (0x20) of the chip (0 if not used).
+* @param[in] reg_ui8 Register to be read
+* @param[out] value_pui16 Pointer to data storage
+*
+* @result 0 = an error occurred, 1 = operation succeeded
+*/
+uint8_t i2c_pca9555_readWord(uint8_t addrOffset_ui8, uint8_t reg_ui8, uint16_t* value_pui16)
 {
   uint8_t result;
 
-#ifdef DEBUG_I2C
-  debug_printf("I2C: pca9555 read word ");
-#endif
+  /* Address the chip for writing */
+  result = i2c_master_select(I2C_SLA_PCA9555 + addrOffset_ui8, TW_WRITE);
 
-  result = i2c_master_select(I2C_SLA_PCA9555 + addrOffset, TW_WRITE);
-
+  /* Send the desired register */
   if (1 == result)
   {
-    TWDR = reg;
+    TWDR = reg_ui8;
     result = (TW_MT_DATA_ACK == i2c_master_transmit_with_ack()) ? 1 : 0;
   }
 
+  /* Address the chip for reading */
   if (1 == result)
   {
-    result = i2c_master_select(I2C_SLA_PCA9555 + addrOffset, TW_READ);
+    result = i2c_master_select(I2C_SLA_PCA9555 + addrOffset_ui8, TW_READ);
   }
 
+  /* Read low byte */
   if (1 == result)
   {
     result = (TW_MR_DATA_ACK == i2c_master_transmit_with_ack()) ? 1 : 0;
-    *value = TWDR;
+    *value_pui16 = TWDR;
   }
 
+  /* Read high byte */
   if (1 == result)
   {
+    /* Do not acknowledge last byte */
     result = (TW_MR_DATA_NACK == i2c_master_transmit()) ? 1 : 0;
-    *value |= (uint16_t) TWDR << 8;
+    *value_pui16 |= (uint16_t) TWDR << 8;
   }
 
 #ifdef DEBUG_I2C
   if (1 == result)
   {
-    debug_printf("%i OK.\n", *value);
+    debug_printf("I2C: pca9555 read word @%i OK. (%i)\n", reg_ui8, *value_pui16);
   }
   else
   {
-    debug_printf("failed.\n");
+    debug_printf("I2C: pca9555 read word @%i failed.\n", reg_ui8);
   }
 #endif
 
@@ -82,49 +95,59 @@ uint8_t i2c_pca9555_readWord(uint8_t addrOffset, uint8_t reg, uint16_t* value)
   return result;
 }
 
-uint8_t i2c_pca9555_readByte(uint8_t addrOffset, uint8_t reg, uint8_t* value)
+/**
+* @brief Read one byte from the chip.
+*
+* Addresses the chip and reads the given register into value_pui8.
+*
+* @param[in] addrOffset_ui8 Offset to the slave address (0x20) of the chip (0 if not used).
+* @param[in] reg_ui8 Register to be read
+* @param[out] value_pui8 Pointer to data storage
+*
+* @result 0 = an error occurred, 1 = operation succeeded
+*/
+uint8_t i2c_pca9555_readByte(uint8_t addrOffset_ui8, uint8_t reg_ui8, uint8_t* value_pui8)
 {
   uint8_t result;
 
-#ifdef DEBUG_I2C
-  debug_printf("I2C: pca9555 read byte ");
-#endif
+  /* Address the chip for writing */
+  result = i2c_master_select(I2C_SLA_PCA9555 + addrOffset_ui8, TW_WRITE);
 
-  result = i2c_master_select(I2C_SLA_PCA9555 + addrOffset, TW_WRITE);
-
+  /* Send the desired register */
   if (1 == result)
   {
-    TWDR = reg;
+    TWDR = reg_ui8;
     result = (TW_MT_DATA_ACK == i2c_master_transmit_with_ack()) ? 1 : 0;
   }
 
   if (1 == result)
   {
-    /* Do an repeated start condition */
+    /* Make a repeated start */
     result = (i2c_master_start() == TW_REP_START) ? 1 : 0;
 
     if (1 == result)
     {
-      /* Send the address again */
+      /* Address the chip for reading */
       TWDR = (I2C_SLA_PCA9555 << 1) | TW_READ;
       result = (i2c_master_transmit() == TW_MR_SLA_ACK) ? 1 : 0;
     }
   }
 
+  /* Read the register */
   if (1 == result)
   {
     result = (TW_MR_DATA_ACK == i2c_master_transmit_with_ack()) ? 1 : 0;
-    *value = TWDR;
+    *value_pui8 = TWDR;
   }
 
 #ifdef DEBUG_I2C
   if (1 == result)
   {
-    debug_printf("%i OK.\n", *value);
+    debug_printf("I2C: pca9555 read byte @%i OK. (%i)\n", reg_ui8, *value_pui8);
   }
   else
   {
-    debug_printf("failed.\n");
+    debug_printf("I2C: pca9555 read byte @%i failed.\n", reg_ui8);
   }
 #endif
 
@@ -133,42 +156,54 @@ uint8_t i2c_pca9555_readByte(uint8_t addrOffset, uint8_t reg, uint8_t* value)
   return result;
 }
 
-uint8_t i2c_pca9555_writeWord(uint8_t addrOffset, uint8_t reg, uint16_t value)
+/**
+* @brief Send one word (16bit) to the chip.
+*
+* Addresses the chip and writes value_ui16 into the low
+* byte and the high byte of the given register.
+*
+* @param[in] addrOffset_ui8 Offset to the slave address (0x20) of the chip (0 if not used).
+* @param[in] reg_ui8 Register to be written.
+* @param[in] value_ui16 Data to be written.
+*
+* @result 0 = an error occurred, 1 = operation succeeded
+*/
+uint8_t i2c_pca9555_writeWord(uint8_t addrOffset_ui8, uint8_t reg_ui8, uint16_t value_ui16)
 {
   uint8_t result = 1;
 
+  /* Address the chip for writing */
+  result = i2c_master_select(I2C_SLA_PCA9555 + addrOffset_ui8, TW_WRITE);
+
+  /* Send the desired register */
+  if (1 == result)
+  {
+    TWDR = reg_ui8;
+    result = (TW_MT_DATA_ACK == i2c_master_transmit_with_ack()) ? 1 : 0;
+  }
+
+  /* Send the low byte */
+  if (1 == result)
+  {
+    TWDR = LO8(value_ui16);
+    result = (TW_MT_DATA_ACK == i2c_master_transmit_with_ack()) ? 1 : 0;
+  }
+
+  /* Send the high byte */
+  if (1 == result)
+  {
+    TWDR = HI8(value_ui16);
+    result = (TW_MT_DATA_ACK == i2c_master_transmit_with_ack()) ? 1 : 0;
+  }
+
 #ifdef DEBUG_I2C
-  debug_printf("I2C: pca9555 write word ");
-#endif
-
-  result = i2c_master_select(I2C_SLA_PCA9555 + addrOffset, TW_WRITE);
-
   if (1 == result)
   {
-    TWDR = reg;
-    result = (TW_MT_DATA_ACK == i2c_master_transmit_with_ack()) ? 1 : 0;
-  }
-
-  if (1 == result)
-  {
-    TWDR = LOW_BYTE(value);
-    result = (TW_MT_DATA_ACK == i2c_master_transmit_with_ack()) ? 1 : 0;
-  }
-
-  if (1 == result)
-  {
-    TWDR = HIGH_BYTE(value);
-    result = (TW_MT_DATA_ACK == i2c_master_transmit_with_ack()) ? 1 : 0;
-  }
-
-#ifdef DEBUG_I2C
-  if (1 == result)
-  {
-    debug_printf("%i OK.\n", value);
+    debug_printf("I2C: pca9555 write word %i @%i OK.\n", value_ui16, reg_ui8);
   }
   else
   {
-    debug_printf("failed\n");
+    debug_printf("I2C: pca9555 write word %i @%i failed\n", reg_ui8);
   }
 #endif
 
@@ -176,36 +211,46 @@ uint8_t i2c_pca9555_writeWord(uint8_t addrOffset, uint8_t reg, uint16_t value)
   return result;
 }
 
-uint8_t i2c_pca9555_writeByte(uint8_t addrOffset, uint8_t reg, uint8_t value)
+/**
+* @brief Send one byte to the chip.
+*
+* Addresses the chip and writes value_ui8 into the given register.
+*
+* @param[in] addrOffset_ui8 Offset to the slave address (0x20) of the chip (0 if not used).
+* @param[in] reg_ui8 Register to be written.
+* @param[in] value_ui8 Data to be written.
+*
+* @result 0 = an error occurred, 1 = operation succeeded
+*/
+uint8_t i2c_pca9555_writeByte(uint8_t addrOffset_ui8, uint8_t reg_ui8, uint8_t value_ui8)
 {
   uint8_t result = 1;
 
-#ifdef DEBUG_I2C
-  debug_printf("I2C: pca9555 write byte ");
-#endif
+  /* Address the chip for writing */
+  result = i2c_master_select(I2C_SLA_PCA9555 + addrOffset_ui8, TW_WRITE);
 
-  result = i2c_master_select(I2C_SLA_PCA9555 + addrOffset, TW_WRITE);
-
+  /* Send the desired register */
   if (1 == result)
   {
-    TWDR = reg;
+    TWDR = reg_ui8;
     result = (TW_MT_DATA_ACK == i2c_master_transmit_with_ack()) ? 1 : 0;
   }
 
+  /* Send the value */
   if (1 == result)
   {
-    TWDR = value;
+    TWDR = value_ui8;
     result = (TW_MT_DATA_ACK == i2c_master_transmit_with_ack()) ? 1 : 0;
   }
 
 #ifdef DEBUG_I2C
   if (1 == result)
   {
-    debug_printf("%i OK.\n", value);
+    debug_printf("I2C: pca9555 write byte %i @%i OK.\n", value_ui8, reg_ui8);
   }
   else
   {
-    debug_printf("failed\n");
+    debug_printf("I2C: pca9555 write byte %i @%i failed\n", reg_ui8);
   }
 #endif
 
--- hardware/i2c/master/i2c_pca9555.h
+++ hardware/i2c/master/i2c_pca9555.h
@@ -22,12 +22,12 @@
 #ifndef _I2C_PCA9555_H
 #define _I2C_PCA9555_H
 
-#define I2C_SLA_PCA9555 0x20
+#define I2C_SLA_PCA9555 0x20 /* Slave address of the PCA9555 */
 
-#define I2C_PCA9555_REG_IN 0
-#define I2C_PCA9555_REG_OUT 2
-#define I2C_PCA9555_REG_POL 4
-#define I2C_PCA9555_REG_CONF 6
+#define I2C_PCA9555_REG_IN 0    /* Input register, low byte */
+#define I2C_PCA9555_REG_OUT 2   /* Output register, low byte */
+#define I2C_PCA9555_REG_POL 4   /* Polarity register, low byte */
+#define I2C_PCA9555_REG_CONF 6  /* Configuration register, low byte */
 
 uint8_t i2c_pca9555_readWord(uint8_t addrOffset, uint8_t reg, uint16_t* value);
 uint8_t i2c_pca9555_writeWord(uint8_t addrOffset, uint8_t reg, uint16_t value);
@@ -35,31 +35,53 @@ uint8_t i2c_pca9555_writeWord(uint8_t addrOffset, uint8_t reg, uint16_t value);
 uint8_t i2c_pca9555_readByte(uint8_t addrOffset, uint8_t reg, uint8_t* value);
 uint8_t i2c_pca9555_writeByte(uint8_t addrOffset, uint8_t reg, uint8_t value);
 
+/* Set and get the data direction register for all pins */
 #define i2c_pca9555_setDDR(addrOffset, value) i2c_pca9555_writeWord(addrOffset, I2C_PCA9555_REG_CONF,  value)
 #define i2c_pca9555_getDDR(addrOffset, valuePtr) i2c_pca9555_readWord(addrOffset, I2C_PCA9555_REG_CONF, valuePtr)
 
+/* Set and get the data direction register for the lower 8 pins */
+#define i2c_pca9555_setDDRL(addrOffset, value) i2c_pca9555_writeByte(addrOffset, I2C_PCA9555_REG_CONF,  value)
+#define i2c_pca9555_getDDRL(addrOffset, valuePtr) i2c_pca9555_reaByte(addrOffset, I2C_PCA9555_REG_CONF, valuePtr)
+
+/* Set and get the data direction register for the higher 8 pins */
+#define i2c_pca9555_setDDRH(addrOffset, value) i2c_pca9555_writeByte(addrOffset, I2C_PCA9555_REG_CONF+1,  value)
+#define i2c_pca9555_getDDRH(addrOffset, valuePtr) i2c_pca9555_readByte(addrOffset, I2C_PCA9555_REG_CONF+1, valuePtr)
+
+/* Set and get the polarity register for all pins */
 #define i2c_pca9555_setPolarity(addrOffset, value) i2c_pca9555_writeWord(addrOffset, I2C_PCA9555_REG_POL,  value)
 #define i2c_pca9555_getPolarity(addrOffset, valuePtr) i2c_pca9555_readWord(addrOffset, I2C_PCA9555_REG_POL, valuePtr)
 
-#define i2c_pca9555_readInPort(addrOffset, valuePtr) i2c_pca9555_readWord(addrOffset, I2C_PCA9555_REG_IN,  valuePtr) //set output word
-#define i2c_pca9555_readOutPort(addrOffset, valuePtr) i2c_pca9555_readWord(addrOffset, I2C_PCA9555_REG_OUT,  valuePtr) //set output word
-#define i2c_pca9555_writeOutPort(addrOffset, value) i2c_pca9555_writeWord(addrOffset, I2C_PCA9555_REG_OUT,  value) //set output word
-
-#define i2c_pca9555_setDDRL(addrOffset, value) i2c_pca9555_writeByte(addrOffset, I2C_PCA9555_REG_CONF,  value)
-#define i2c_pca9555_setDDRH(addrOffset, value) i2c_pca9555_writeByte(addrOffset, I2C_PCA9555_REG_CONF+1,  value)
-#define i2c_pca9555_getDDRL(addrOffset, valuePtr) i2c_pca9555_reaByte(addrOffset, I2C_PCA9555_REG_CONF, valuePtr)
-#define i2c_pca9555_getDDRH(addrOffset, valuePtr) i2c_pca9555_readByte(addrOffset, I2C_PCA9555_REG_CONF+1, valuePtr)
-
+/* Set and get the polarity register for the lower 8 pins */
 #define i2c_pca9555_setPolarityL(addrOffset, value) i2c_pca9555_writeByte(addrOffset, I2C_PCA9555_REG_POL,  value)
-#define i2c_pca9555_setPolarityH(addrOffset, value) i2c_pca9555_writeByte(addrOffset, I2C_PCA9555_REG_POL+1,  value)
 #define i2c_pca9555_getPolarityL(addrOffset, valuePtr) i2c_pca9555_readByte(addrOffset, I2C_PCA9555_REG_POL, valuePtr)
+
+/* Set and get the polarity register for the higher 8 pins */
+#define i2c_pca9555_setPolarityH(addrOffset, value) i2c_pca9555_writeByte(addrOffset, I2C_PCA9555_REG_POL+1,  value)
 #define i2c_pca9555_getPolarityH(addrOffset, valuePtr) i2c_pca9555_readByte(addrOffset, I2C_PCA9555_REG_POL+1, valuePtr)
 
-#define i2c_pca9555_readInPortL(addrOffset, valuePtr) i2c_pca9555_readByte(addrOffset, I2C_PCA9555_REG_IN,  valuePtr) //set output word
-#define i2c_pca9555_readInPortH(addrOffset, valuePtr) i2c_pca9555_readByte(addrOffset, I2C_PCA9555_REG_IN+1,  valuePtr) //set output word
-#define i2c_pca9555_readOutPortL(addrOffset, valuePtr) i2c_pca9555_readByte(addrOffset, I2C_PCA9555_REG_OUT,  valuePtr) //set output word
-#define i2c_pca9555_readOutPortH(addrOffset, valuePtr) i2c_pca9555_readByte(addrOffset, I2C_PCA9555_REG_OUT+1,  valuePtr) //set output word
-#define i2c_pca9555_writeOutPortL(addrOffset, value) i2c_pca9555_writeByte(addrOffset, I2C_PCA9555_REG_OUT,  value) //set output word
-#define i2c_pca9555_writeOutPortH(addrOffset, value) i2c_pca9555_writeByte(addrOffset, I2C_PCA9555_REG_OUT+1,  value) //set output word
+/* Read the input register for all pins */
+#define i2c_pca9555_readInPort(addrOffset, valuePtr) i2c_pca9555_readWord(addrOffset, I2C_PCA9555_REG_IN,  valuePtr)
+
+/* Read and write the output register for all pins */
+#define i2c_pca9555_readOutPort(addrOffset, valuePtr) i2c_pca9555_readWord(addrOffset, I2C_PCA9555_REG_OUT,  valuePtr)
+#define i2c_pca9555_writeOutPort(addrOffset, value) i2c_pca9555_writeWord(addrOffset, I2C_PCA9555_REG_OUT,  value)
+
+/* Read the input register for the lower 8 pins */
+#define i2c_pca9555_readInPortL(addrOffset, valuePtr) i2c_pca9555_readByte(addrOffset, I2C_PCA9555_REG_IN,  valuePtr)
+
+/* Read the input register for the higher 8 pins */
+#define i2c_pca9555_readInPortH(addrOffset, valuePtr) i2c_pca9555_readByte(addrOffset, I2C_PCA9555_REG_IN+1,  valuePtr)
+
+/* Read the output register for the lower 8 pins */
+#define i2c_pca9555_readOutPortL(addrOffset, valuePtr) i2c_pca9555_readByte(addrOffset, I2C_PCA9555_REG_OUT,  valuePtr)
+
+/* Read the output register for the higher 8 pins */
+#define i2c_pca9555_readOutPortH(addrOffset, valuePtr) i2c_pca9555_readByte(addrOffset, I2C_PCA9555_REG_OUT+1,  valuePtr)
+
+/* Write the output register for the lower 8 pins */
+#define i2c_pca9555_writeOutPortL(addrOffset, value) i2c_pca9555_writeByte(addrOffset, I2C_PCA9555_REG_OUT,  value)
+
+/* Write the output register for the higher 8 pins */
+#define i2c_pca9555_writeOutPortH(addrOffset, value) i2c_pca9555_writeByte(addrOffset, I2C_PCA9555_REG_OUT+1,  value)
 
 #endif /* _I2C_PCA9555_H */
