6e87f7b020405406493cc5c96704d86dfd6c2085
Maximilian Güntner <maximilian.guentner@gmail.com>
2012-01-06 00:23:46 +0100

    fixed wrong sscanf arguments and removed static variables

--- services/dmx-storage/dmx_storage_ecmd.c
+++ services/dmx-storage/dmx_storage_ecmd.c
@@ -30,8 +30,9 @@
 #ifdef DMX_STORAGE_SUPPORT
 int16_t parse_cmd_dmx_get_channel(char *cmd, char *output, uint16_t len)
 {
-	uint16_t ret=0, channel=0, universe=0;
-	if (cmd[0]!=0) ret = sscanf_P(cmd, PSTR("%u %u"), &universe, &channel);
+	uint16_t ret=0, channel=0;
+	uint8_t  universe=0;
+	if (cmd[0]!=0) ret = sscanf_P(cmd, PSTR("%hu %hhu"), &universe, &channel);
 	if(ret == 2)
 	{
 		if (channel >= DMX_STORAGE_CHANNELS)
@@ -47,9 +48,10 @@ int16_t parse_cmd_dmx_get_channel(char *cmd, char *output, uint16_t len)
 
 int16_t parse_cmd_dmx_set_channels(char *cmd, char *output, uint16_t len)
 {
-	uint16_t startchannel=0, universe=0, value=0, channelcounter=0,i=0,blankcounter=0;
+	uint16_t startchannel=0, value=0, channelcounter=0, blankcounter=0;
+	uint8_t universe=0, i=0;
 	if (cmd[0]!=0) {
-		sscanf_P(cmd, PSTR("%u %u"), &universe,&startchannel);
+		sscanf_P(cmd, PSTR("%hhu %hu"), &universe, &startchannel);
 		if (startchannel >= DMX_STORAGE_CHANNELS)
 			return ECMD_ERR_PARSE_ERROR;
 		if (universe >= DMX_STORAGE_UNIVERSES)
@@ -88,36 +90,39 @@ int16_t parse_cmd_dmx_universes(char *cmd, char *output, uint16_t len)
 }
 int16_t parse_cmd_dmx_get_universe(char *cmd, char *output, uint16_t len)
 {
-	uint16_t ret=0, universe=0;
-	uint8_t value=0;
-	if (cmd[0]!=0) ret = sscanf_P(cmd, PSTR("%u"), &universe);
-	if(ret == 1 && universe < DMX_STORAGE_UNIVERSES)
+	uint16_t ret=0;
+	uint8_t value=0, universe=0;
+	/* trick: use bytes on cmd as "connection specific static variables" */
+	if (cmd[0] != 23)       /* indicator flag: real invocation:  0 */
 	{
-		ret=0;
-		static uint16_t chan = 0;
-		value=get_dmx_channel(universe,chan);
-		output[ret+2] = value%10 +48;
-		value /= 10;
-		output[ret+1] = value%10 +48;
-		value /= 10;
-		output[ret+0] = value%10 +48;
-		ret+=3;
-		if(chan < DMX_STORAGE_CHANNELS-1)
-		{
-			chan++;
-			return ECMD_AGAIN(ret);
-		}
-		else
-		{
-			chan=0;
-			return ECMD_FINAL(ret);
-		}
-
+		/* read universe */
+		ret = sscanf_P(cmd, PSTR("%hhu"), &universe);
+		if(ret != 1 || universe >= DMX_STORAGE_UNIVERSES)
+			return ECMD_ERR_PARSE_ERROR;
+		cmd[0] = 23;    	/* continuing call: 23 */
+		cmd[1] = universe; 	/* universe */
+		cmd[2] = 0;		/* reserved for chan */
+		cmd[3] = 0;		/* reserved for chan */
+	}
+	ret=0;
+	universe=cmd[1];
+	uint16_t chan = *((uint16_t *)(cmd)+1);
+	value=get_dmx_channel(universe,chan);
+	output[ret+2] = value%10 +48;
+	value /= 10;
+	output[ret+1] = value%10 +48;
+	value /= 10;
+	output[ret+0] = value%10 +48;
+	ret+=3;
+	if(chan < DMX_STORAGE_CHANNELS-1)
+	{
+		chan++;
+		*((uint16_t *)(cmd)+1)=chan;
+		return ECMD_AGAIN(ret);
 	}
 	else
-		return ECMD_ERR_PARSE_ERROR;
+		return ECMD_FINAL(ret);
 }
-
 #endif
 /*
    -- Ethersex META --
