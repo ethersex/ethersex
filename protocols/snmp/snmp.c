bd2838c7ebe36616c3af823531a96c2602396974
Sascha Ittner <sascha.ittner@modusoft.de>
2012-02-28 20:21:53 +0100

    snmp-fixes

--- protocols/snmp/snmp.c
+++ protocols/snmp/snmp.c
@@ -1,6 +1,7 @@
 /*
  *
  * Copyright (c) 2008 by Christian Dietrich <stettberger@dokucode.de>
+ * Copyright (c) 2012 by Sascha Ittner <sascha.ittner@modusoft.de>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -23,23 +24,22 @@
 #include <avr/io.h>
 #include <avr/pgmspace.h>
 #include "config.h"
-#include "protocols/uip/uip.h"
-#include "protocols/uip/uip_router.h"
 #include "core/debug.h"
 #include "core/bit-macros.h"
 #include "services/clock/clock.h"
-#include "snmp_net.h"
 #include "snmp.h"
 
 
 #ifdef SNMP_SUPPORT
 
-#define BUF ((struct uip_udpip_hdr *) (uip_appdata - UIP_IPUDPH_LEN))
-
 #ifdef WHM_SUPPORT
 uint8_t
-uptime_reaction(uint8_t *ptr, struct snmp_varbinding *bind, void *userdata)
+uptime_reaction(uint8_t * ptr, struct snmp_varbinding *bind, void *userdata)
 {
+  if (bind->len != 0)
+  {
+    return 0;
+  }
   uint32_t seconds = clock_get_uptime();
   /* This long long (uint64_t) hack is necessary, because it seems to be, that
    * seconds = seconds * 100 doesn't work at all
@@ -47,9 +47,9 @@ uptime_reaction(uint8_t *ptr, struct snmp_varbinding *bind, void *userdata)
   seconds = seconds * 100LL;
   uint32_t *time_ptr = (void *) ptr + 2;
 
-  ptr[0] = 0x43;
+  ptr[0] = SNMP_TYPE_TIMETICKS;
   ptr[1] = 4;
-  *time_ptr = HTONL((uint32_t)seconds);
+  *time_ptr = HTONL((uint32_t) seconds);
 
   return 6;
 }
@@ -57,32 +57,55 @@ uptime_reaction(uint8_t *ptr, struct snmp_varbinding *bind, void *userdata)
 
 #ifdef ADC_SUPPORT
 uint8_t
-adc_reaction(uint8_t *ptr, struct snmp_varbinding *bind, void *userdata)
+adc_reaction(uint8_t * ptr, struct snmp_varbinding * bind, void *userdata)
 {
-  ptr[0] = 2;
-  ptr[1] = 2;
-  if ( bind->len > 0 && bind->data[0] < ADC_CHANNELS) {
-    ADMUX = (ADMUX & 0xF0) | bind->data[0];
-    /* Start adc conversion */
-    ADCSRA |= _BV(ADSC);
-    /* Wait for completion of adc */
-    while (ADCSRA & _BV(ADSC)) {}
-    uint16_t adc = ADC;
-    ptr[2] = adc >> 8;
-    ptr[3] = adc & 0xff;
-  } else {
-    ptr[2] = ptr[3] = 0;
+  if (bind->len != 1 || bind->data[0] >= ADC_CHANNELS)
+  {
+    return 0;
   }
+  ADMUX = (ADMUX & 0xF0) | bind->data[0];
+  /* Start adc conversion */
+  ADCSRA |= _BV(ADSC);
+  /* Wait for completion of adc */
+  while (ADCSRA & _BV(ADSC))
+  {
+  }
+  uint16_t adc = ADC;
+
+  ptr[0] = SNMP_TYPE_INTEGER;
+  ptr[1] = 2;
+  ptr[2] = adc >> 8;
+  ptr[3] = adc & 0xff;
 
   return 4;
+}
+
+uint8_t
+adc_next(uint8_t * ptr, struct snmp_varbinding * bind)
+{
+  if (bind->len == 0)
+  {
+    ptr[0] = 0;
+    return 1;
+  }
+  if (bind->len == 1 && bind->data[0] < (ADC_CHANNELS - 1))
+  {
+    ptr[0] = bind->data[0] + 1;
+    return 1;
+  }
+  return 0;
 }
 #endif
 
 uint8_t
-string_pgm_reaction(uint8_t *ptr, struct snmp_varbinding *bind, void *userdata)
+string_pgm_reaction(uint8_t * ptr, struct snmp_varbinding * bind,
+                    void *userdata)
 {
-  (void) bind;
-  ptr[0] = 4;
+  if (bind->len != 0)
+  {
+    return 0;
+  }
+  ptr[0] = SNMP_TYPE_STRING;
   ptr[1] = strlen_P((char *) userdata);
   memcpy_P(ptr + 2, userdata, ptr[1]);
   return ptr[1] + 2;
@@ -91,7 +114,9 @@ string_pgm_reaction(uint8_t *ptr, struct snmp_varbinding *bind, void *userdata)
 const char desc_value[] PROGMEM = SNMP_VALUE_DESCRIPTION;
 const char desc_obj_name[] PROGMEM = "\x2b\x06\x01\x02\x01\x01\x01";
 
+#ifdef WHM_SUPPORT
 const char uptime_reaction_obj_name[] PROGMEM = "\x2b\x06\x01\x02\x01\x01\x03";
+#endif
 
 const char contact_value[] PROGMEM = SNMP_VALUE_CONTACT;
 const char contact_obj_name[] PROGMEM = "\x2b\x06\x01\x02\x01\x01\x04";
@@ -102,166 +127,22 @@ const char hostname_value[] PROGMEM = CONF_HOSTNAME;
 const char location_value[] PROGMEM = SNMP_VALUE_LOCATION;
 const char location_obj_name[] PROGMEM = "\x2b\x06\x01\x02\x01\x01\x06";
 
-const char adc_reaction_obj_name[] PROGMEM = ethersexExperimental "\01";
-
-static struct snmp_reaction snmp_reactions[] = {
 #ifdef ADC_SUPPORT
-  {adc_reaction_obj_name, adc_reaction, NULL},
+const char adc_reaction_obj_name[] PROGMEM = ethersexExperimental "\x01";
 #endif
-  {desc_obj_name, string_pgm_reaction, (void *)desc_value},
+
+const struct snmp_reaction snmp_reactions[] PROGMEM = {
+  {desc_obj_name, string_pgm_reaction, (void *) desc_value, NULL},
 #ifdef WHM_SUPPORT
-  {uptime_reaction_obj_name, uptime_reaction, NULL},
+  {uptime_reaction_obj_name, uptime_reaction, NULL, NULL},
 #endif
-  {contact_obj_name, string_pgm_reaction, (void *)contact_value},
-  {hostname_reaction_obj_name, string_pgm_reaction, (void *)hostname_value},
-  {location_obj_name, string_pgm_reaction, (void *)location_value},
-  {NULL, NULL, 0}
+  {contact_obj_name, string_pgm_reaction, (void *) contact_value, NULL},
+  {hostname_reaction_obj_name, string_pgm_reaction, (void *) hostname_value, NULL},
+  {location_obj_name, string_pgm_reaction, (void *) location_value, NULL},
+#ifdef ADC_SUPPORT
+  {adc_reaction_obj_name, adc_reaction, NULL, adc_next},
+#endif
+  {NULL, NULL, NULL, NULL}
 };
 
-void
-snmp_new_data(void)
-{
-  struct snmp_packet pkt;
-  uint8_t pdu_type;
-  uint8_t tmp, *request = (uint8_t *)uip_appdata;
-  uint16_t ptr = 0;
-  /* Parse the packet */
-  ptr += 4; /* Consume packet encapsulation and version field */
-  pkt.version = request[ptr];
-  /* Community string */
-  ptr += 2;
-  tmp = request[ptr]; /* Community string length */
-  pkt.community = &request[ptr + 1];
-  pkt.pdu_type = &request[ptr + 1 + tmp]; /* We must save the pdu type, becaus
-                                          it is overwritten in the next step */
-  pdu_type = pkt.pdu_type[0];
-
-  pkt.community[tmp] = 0;
-  ptr +=  1 + tmp + 4;
-  memcpy(&pkt.request_id, &request[ptr], 4);
-  ptr += 11; /* Skip error status, error index and the varbind encapsulation*/
-  uint8_t *varbind_start = request + ptr - 1;
-
-  uint16_t x, var_len = request[ptr];
-  uint8_t y;
-
-  pkt.var_count = 0;
-  ptr += 2;
-  for(x = 0; x != var_len;) {
-    x += 2 + request[ptr + x];
-    pkt.var_count++;
-  }
-  pkt.binds = __builtin_alloca(pkt.var_count * sizeof(struct snmp_varbinding));
-  for(tmp = 0, x = 0; x < var_len;) {
-    pkt.binds[tmp].len = request[ptr + x + 2];
-    pkt.binds[tmp].data = &request[ptr + x + 3];
-    tmp++;
-
-    x += 2 + request[ptr + x];
-  }
-
-  /* interpret the different mibs */
-  uint8_t z;
-  for (y = 0; y < pkt.var_count; y++) {
-    pkt.binds[y].type = 0xff;
-
-    for (z = 0; snmp_reactions[z].obj_name; z++) {
-  /*    syslog_sendf("%d %d; %d %x : %d %x | ", y, z,
-                   pkt.binds[y].len, pkt.binds[y].data[0],
-                   strlen((char *)snmp_reactions[z].obj_name)
-                          + snmp_reactions[z].append_zero,
-                          snmp_reactions[z].obj_name[0]); */
-
-      uint8_t store_len = strlen_P((char *)snmp_reactions[z].obj_name);
-      uint8_t request_len = pkt.binds[y].len;
-      uint8_t cmp_len = store_len > request_len ? request_len : store_len;
-
-      debug_printf("d: len %d, rlen %d\n", store_len, request_len);
-      if (memcmp_P(pkt.binds[y].data, snmp_reactions[z].obj_name, cmp_len) == 0) {
-        pkt.binds[y].type = z;
-        if (pdu_type == 0xa1 && request_len == store_len) {
-          if (snmp_reactions[z+1].obj_name) {
-            pkt.binds[y].type += 1;
-          } else {
-            pkt.binds[y].type = 0xff;
-          }
-        }
-        if (store_len < pkt.binds[y].len) {
-          uint8_t *data_ptr = pkt.binds[y].data;
-          pkt.binds[y].len = pkt.binds[y].len - store_len;
-          pkt.binds[y].data = __builtin_alloca(pkt.binds[y].len);
-          memcpy(pkt.binds[y].data, data_ptr + store_len,
-                 pkt.binds[y].len);
-        } else {
-          pkt.binds[y].data = NULL;
-          pkt.binds[y].len = 0;
-        }
-
-        break;
-      }
-    }
-  }
-
-  /* We assemble the packet within the received packet */
-  pkt.pdu_type[0] = 0xa2; /* Our packet type is an get-response packet */
-  request[1] = pkt.pdu_type - request;
-
-  varbind_start[0] = 0x30; /* Varbind encapsulation */
-  varbind_start[1] = 0;
-
-  uint8_t *bind_start = varbind_start + 2;
-  for (ptr = 0; ptr < pkt.var_count; ptr++) {
-    uint8_t type = pkt.binds[ptr].type;
-    if (type == 0xff) {
-      /* This datatype is not defined */
-      pkt.pdu_type[1 + 2 + 4 + 3] = 2; /* NoSuchName */;
-      pkt.pdu_type[1 + 2 + 4 + 3 + 3 ] = ptr; /* Error in this Node */;
-      varbind_start[1] = 0;
-      break;
-    }
-    bind_start[0] = 0x30;
-    bind_start[1] = 2 + strlen_P((char *)snmp_reactions[type].obj_name);
-    /* Object identifier */
-    bind_start[2] = 0x06; /* Object identifier start */
-    bind_start[3] = bind_start[1] - 2;
-    memcpy_P(bind_start + 4, snmp_reactions[type].obj_name, bind_start[3]);
-
-    /* Variable part  of the object identifier*/
-    memcpy(bind_start + 4 + bind_start[3], pkt.binds[ptr].data,
-           pkt.binds[ptr].len);
-    bind_start[3] += pkt.binds[ptr].len;
-    bind_start[1] += pkt.binds[ptr].len;
-
-    /* Append the value */
-    bind_start[1] += snmp_reactions[type].cb(bind_start + 4 + bind_start[3],
-                                             &pkt.binds[ptr],
-                                             snmp_reactions[type].userdata);
-
-    varbind_start[1] += bind_start[1] + 2;
-
-    bind_start += bind_start[1] + 2;
-  }
-
-  pkt.pdu_type[1] = varbind_start[1] + 14;
-  request[1] += pkt.pdu_type[1];
-
-
-  uip_udp_send(request[1] + 2);
-  /* Send the packet */
-  uip_udp_conn_t conn;
-  uip_ipaddr_copy(conn.ripaddr, BUF->srcipaddr);
-  conn.rport = BUF->srcport;
-  conn.lport = HTONS(SNMP_PORT);
-
-  uip_udp_conn = &conn;
-
-  /* Send immediately */
-  uip_process(UIP_UDP_SEND_CONN);
-  router_output();
-
-  uip_slen = 0;
-
-}
-
 #endif
-
--- protocols/snmp/snmp_net.c
+++ protocols/snmp/snmp_net.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2007 by Christian Dietrich <stettberger@dokucode.de>
+ * Copyright (c) 2012 by Sascha Ittner <sascha.ittner@modusoft.de>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -20,11 +21,15 @@
  */
 
 #include <avr/io.h>
+#include <avr/pgmspace.h>
 
 #include "protocols/uip/uip.h"
+#include "protocols/uip/uip_router.h"
 #include "config.h"
 #include "snmp.h"
 #include "snmp_net.h"
+
+#define BUF ((struct uip_udpip_hdr *) (uip_appdata - UIP_IPUDPH_LEN))
 
 void
 snmp_net_init(void)
@@ -33,17 +38,342 @@ snmp_net_init(void)
   uip_ipaddr_t ip;
   uip_ipaddr_copy(&ip, all_ones_addr);
 
-  if(! (conn = uip_udp_new(&ip, 0, snmp_net_main)))
-    return; /* Couldn't bind socket */
+  if (!(conn = uip_udp_new(&ip, 0, snmp_net_main)))
+    return;                     /* Couldn't bind socket */
 
   uip_udp_bind(conn, HTONS(SNMP_PORT));
+}
+
+const struct snmp_reaction *
+snmp_find_reaction(uint8_t * oid, uint8_t oid_len,
+                   struct snmp_varbinding *bind)
+{
+  const struct snmp_reaction *z;
+  const char *obj_name;
+  for (z = snmp_reactions;
+       (obj_name = (const char *) pgm_read_word(&z->obj_name)) != NULL; z++)
+  {
+    uint8_t store_len = strlen_P(obj_name);
+    uint8_t cmp_len, len;
+    if (store_len > oid_len)
+    {
+      cmp_len = oid_len;
+      len = 0;
+    }
+    else
+    {
+      cmp_len = store_len;
+      len = oid_len - store_len;
+    }
+
+    if (memcmp_P(oid, obj_name, cmp_len) == 0)
+    {
+      bind->store_len = store_len;
+      bind->len = len;
+      bind->data = oid + store_len;
+      return z;
+    }
+  }
+  return NULL;
+}
+
+uint8_t
+snmp_proc_bind(uint8_t req_type, uint8_t * oid, uint8_t oid_len,
+               uint8_t * out)
+{
+  struct snmp_varbinding bind;
+  const struct snmp_reaction *reaction;
+  uint8_t ret;
+
+  /* search reaction */
+  if (oid_len == 0 && req_type == SNMP_TYPE_GETNEXTREQ)
+  {
+    reaction = snmp_reactions;
+  }
+  else
+  {
+    reaction = snmp_find_reaction(oid, oid_len, &bind);
+  }
+  if (reaction == NULL)
+  {
+    return 0;
+  }
+
+  /* add varbind to output */
+  uint8_t *vb = out;
+  *(out++) = SNMP_TYPE_SEQUENCE;
+  *(out++) = 0;
+
+  if (req_type == SNMP_TYPE_GETNEXTREQ)
+  {
+    /* try to find next sub OID of matching record */
+    snmp_next_callback_t ncb =
+      (snmp_next_callback_t) pgm_read_word(&reaction->ncb);
+    if (ncb != NULL)
+    {
+      uint8_t *new = out + 2 + bind.store_len;
+      bind.len = ncb(new, &bind);
+      bind.data = new;
+    }
+
+    /* no next sub OID -> try next record */
+    if (bind.len == 0)
+    {
+      reaction++;
+      const char *obj_name =
+        (const char *) pgm_read_word(&reaction->obj_name);
+      if (obj_name == NULL)
+      {
+        return 0;
+      }
+      bind.store_len = strlen_P(obj_name);
+      bind.len = 0;
+      bind.data = out + 2 + bind.store_len;
+
+      ncb = (snmp_next_callback_t) pgm_read_word(&reaction->ncb);
+      if (ncb != NULL)
+      {
+        bind.len = ncb(bind.data, &bind);
+      }
+    }
+
+    /* claculate OID lenhth */
+    oid_len = bind.store_len + bind.len;
+
+    /* add oid to output */
+    const char *obj_name = (const char *) pgm_read_word(&reaction->obj_name);
+    *(out++) = SNMP_TYPE_OID;
+    *(out++) = oid_len;
+    memcpy_P(out, obj_name, bind.store_len);
+    out += oid_len;
+  }
+  else
+  {
+    /* check OID length */
+    if (oid_len < bind.store_len)
+    {
+      return 0;
+    }
+
+    /* add oid to output */
+    *(out++) = SNMP_TYPE_OID;
+    *(out++) = oid_len;
+    memcpy(out, oid, oid_len);
+    out += oid_len;
+  }
+
+  /* process reaction */
+  snmp_reaction_callback_t cb =
+    (snmp_reaction_callback_t) pgm_read_word(&reaction->cb);
+  void *data = (void *) pgm_read_word(&reaction->userdata);
+  ret = cb(out, &bind, data);
+  if (ret < 2)
+  {
+    return 0;
+  }
+
+  /* return length */
+  vb[1] = 2 + oid_len + ret;
+  return 2 + vb[1];
 }
 
 void
 snmp_net_main(void)
 {
-  if (uip_newdata())
-    snmp_new_data();
+  uint8_t *req = (uint8_t *) uip_appdata;
+  int16_t len = uip_len;
+
+  if (!uip_newdata())
+  {
+    return;
+  }
+
+  /* check sequence type and length */
+  len -= 2;
+  uint8_t *req_hdr = req;
+  if (len < 0 || *(req++) != SNMP_TYPE_SEQUENCE)
+  {
+    return;
+  }
+  if (*(req++) != len)
+  {
+    return;
+  }
+
+  /* check version */
+  len -= 3;
+  if (len < 0 || *(req++) != SNMP_TYPE_INTEGER || *(req++) != 1 ||
+      *(req++) != SNMP_VERSION1_VALUE)
+  {
+    return;
+  }
+
+  /* check community string */
+  len -= 2;
+  if (len < 0 || *(req++) != SNMP_TYPE_STRING)
+  {
+    return;
+  }
+  uint8_t cs_len = *(req++);
+  if (cs_len > len)
+  {
+    return;
+  }
+  len -= cs_len;
+  uint8_t *cs_ref = (uint8_t *) PSTR(SNMP_COMMUNITY_STRING);
+  while (cs_len > 0)
+  {
+    if (pgm_read_byte(cs_ref++) != *(req++))
+    {
+      return;
+    }
+    cs_len--;
+  }
+  if (pgm_read_byte(cs_ref) != 0)
+  {
+    return;
+  }
+
+  /* check request type and length */
+  len -= 2;
+  uint8_t *req_type = req++;
+  if (len < 0 ||
+      (*req_type != SNMP_TYPE_GETREQ && *req_type != SNMP_TYPE_GETNEXTREQ))
+  {
+    return;
+  }
+  if (*(req++) != len)
+  {
+    return;
+  }
+
+  /* skip request id */
+  len -= 2;
+  if (len < 0 || *(req++) != SNMP_TYPE_INTEGER)
+  {
+    return;
+  }
+  uint8_t id_len = *(req++);
+  if (id_len > len)
+  {
+    return;
+  }
+  req += id_len;
+  len -= id_len;
+
+  /* skip error */
+  len -= 3;
+  if (len < 0 || *(req++) != SNMP_TYPE_INTEGER || *(req++) != 1)
+  {
+    return;
+  }
+  uint8_t *err = req++;
+
+  /* skip error index */
+  len -= 3;
+  if (len < 0 || *(req++) != SNMP_TYPE_INTEGER || *(req++) != 1)
+  {
+    return;
+  }
+  req++;
+
+  /* get varbind list header */
+  len -= 2;
+  uint8_t *vb_list_hdr = req;
+  if (len < 0 || *(req++) != SNMP_TYPE_SEQUENCE)
+  {
+    return;
+  }
+  uint8_t vb_list_len = *(req++);
+  if (vb_list_len != len || vb_list_len > SNMP_MAX_OID_BUFFERSIZE)
+  {
+    return;
+  }
+
+  /* copy varbind list since we build the answer in place */
+  uint8_t *vb_list = __builtin_alloca(vb_list_len);
+  memcpy(vb_list, req, vb_list_len);
+
+  /* process bindings */
+  *err = SNMP_ERR_NONE;
+  int8_t len_delta = 0;
+  uint8_t bind_count = 0;
+  while (len > 0)
+  {
+    /* check maximum bind count */
+    if ((bind_count++) > SNMP_MAX_BIND_COUNT)
+    {
+      return;
+    }
+
+    /* check varbind */
+    len -= 2;
+    if (len < 0 || *(vb_list++) != SNMP_TYPE_SEQUENCE)
+    {
+      return;
+    }
+    uint8_t vb_len = *(vb_list++);
+    if (vb_len > len)
+    {
+      return;
+    }
+    len -= vb_len;
+    int16_t vb_rem = vb_len;
+
+    /* check OID */
+    vb_rem -= 2;
+    if (vb_rem < 0 || *(vb_list++) != SNMP_TYPE_OID)
+    {
+      return;
+    }
+    uint8_t oid_len = *(vb_list++);
+    if (oid_len > vb_rem)
+    {
+      return;
+    }
+    uint8_t *oid = vb_list;
+    vb_list += oid_len;
+    vb_rem -= oid_len;
+
+    /* check null value */
+    if (vb_rem != 2 || *(vb_list++) != SNMP_TYPE_NULL || *(vb_list++) != 0)
+    {
+      return;
+    }
+
+    /* process OID */
+    uint8_t ret = snmp_proc_bind(*req_type, oid, oid_len, req);
+    req += ret;
+    if (ret == 0)
+    {
+      *err = SNMP_ERR_NO_SUCH_NAME;
+    }
+
+    /* update lengths */
+    len_delta += (int8_t) (ret - vb_len - 2);
+  }
+
+  /* set response type */
+  req_type[0] = SNMP_TYPE_GETRESP;
+
+  /* update lengths */
+  req_hdr[1] += len_delta;
+  req_type[1] += len_delta;
+  vb_list_hdr[1] += len_delta;
+
+  uip_udp_conn_t conn;
+  uip_ipaddr_copy(conn.ripaddr, BUF->srcipaddr);
+  conn.rport = BUF->srcport;
+  conn.lport = HTONS(SNMP_PORT);
+
+  uip_udp_conn = &conn;
+
+  /* Send immediately */
+  uip_slen = uip_len + len_delta;
+  uip_process(UIP_UDP_SEND_CONN);
+  router_output();
+
+  uip_slen = 0;
 }
 
 /*
