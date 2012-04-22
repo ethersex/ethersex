4fb24641824c21e171f770caab68d2ed499cb5d2
Maximilian Güntner <maximilian.guentner@gmail.com>
2012-04-22 22:26:50 +0200

    print usage
    Signed-off-by: Maximilian Güntner <maximilian.guentner@gmail.com>

--- contrib/colorcorrection/generate.py
+++ contrib/colorcorrection/generate.py
@@ -14,6 +14,18 @@ import math
 import sys
 pwm_size=4096.00
 pwm_levels=255
+
+def printUsage():
+  print('''usage: generate.py OPTION
+ OPTION: 
+ s: Generates a lookup table of the Stevens\' power law to be copy pasted directly in to c/cpp source code
+ c: Generates a lookup table of the CIELUV (CIE 1931) color space to be copy pasted directly in to c/cpp source code
+
+ For more information, see
+ (1) http://en.wikipedia.org/wiki/CIELUV
+ (2) http://en.wikipedia.org/wiki/Stevens_power_law
+''')
+
 def stevensPowerLaw():
   print('datatype stevens_power[%d] = {' % (pwm_levels+1), end=" ")
   for i in range(0,pwm_levels+1):
@@ -42,8 +54,13 @@ def cieLuminance():
     else:
       print('%d };' % result)
 
+if len(sys.argv) < 2:
+  printUsage()
+  sys.exit(0)
 if sys.argv[1] == "s":
   stevensPowerLaw()
 elif sys.argv[1] == "c":
   cieLuminance()
-
+else:
+  printUsage()
+sys.exit(0)
