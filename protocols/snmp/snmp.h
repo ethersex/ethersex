71b1a2a1c92c718562c3580d906a53abf1362575
Sascha Ittner <sascha.ittner@modusoft.de>
2012-05-30 22:05:10 +0200

    PEN added

--- protocols/snmp/snmp.h
+++ protocols/snmp/snmp.h
@@ -65,15 +65,15 @@
 /* OID: 1.3.6.1.4.1. */
 #define SNMP_OID_ENTERPRISES "\x2b\x06\x01\x04\x01"
 
-/* OID: 1.3.6.1.4.1.2021.13.23. */
+/* OID: 1.3.6.1.4.1.2021.13. */
 #define SNMP_OID_UCDEXPERIMENTAL "\x2b\x06\x01\x04\x01\x8f\x65\x0d"
 
 #ifdef SNMP_EXPERIMENTAL_TREE_SUPPORT
 /* OID: 1.3.6.1.4.1.2021.13.23. */
 #define SNMP_OID_ETHERSEX SNMP_OID_UCDEXPERIMENTAL "\x17"
 #else
-/* OID: 1.3.6.1.4.1. */
-#define SNMP_OID_ETHERSEX SNMP_OID_ENTERPRISES "\x17"
+/* OID: 1.3.6.1.4.1.39967 */
+#define SNMP_OID_ETHERSEX SNMP_OID_ENTERPRISES "\x9f\xb8\x02"
 #endif
 
 struct snmp_varbinding
