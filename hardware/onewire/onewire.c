3e73b60581b80438b98ba2c1a5469d739f8587aa
Frank Sautter <github@sautter.com>
2012-10-02 12:04:34 +0200

    added 'ow_find_sensor_index' function

--- hardware/onewire/onewire.c
+++ hardware/onewire/onewire.c
@@ -588,14 +588,18 @@ ow_sensor_t *
 ow_find_sensor(ow_rom_code_t * rom)
 {
   for (uint8_t i = 0; i < OW_SENSORS_COUNT; i++)
-  {
     if (ow_sensors[i].ow_rom_code.raw == rom->raw)
-    {
-      /* found it */
-      return &ow_sensors[i];
-    }
-  }
+      return &ow_sensors[i];  /* found it */
   return NULL;
+}
+
+int8_t
+ow_find_sensor_index(ow_rom_code_t * rom)
+{
+  for (uint8_t i = 0; i < OW_SENSORS_COUNT; i++)
+    if (ow_sensors[i].ow_rom_code.raw == rom->raw)
+      return i; /* found it */
+  return -1;
 }
 #endif
 
