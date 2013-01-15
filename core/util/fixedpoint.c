3aaf6b714be272e1828897005820ffc7fd43793c
Oliver Wagner <owagner@vapor.com>
2013-01-15 23:41:17 +0100

    - itoa_fixedpoint would not create a leading 0 for decimal-only values
      itoa_fixedpoint(-9,1,buff)
    
      Before: -.9
      After: -0.9

--- core/util/fixedpoint.c
+++ core/util/fixedpoint.c
@@ -55,7 +55,10 @@ uint8_t itoa_fixedpoint(int16_t n, uint8_t fixeddigits, char s[])
         while(i < fixeddigits)
             s[i++]='0';
         s[i++]='.';
+        s[i++]='0';
     }
+    else if(i == fixeddigits)
+        s[i++]='0';
 
     if (sign)
         s[i++] = '-';
