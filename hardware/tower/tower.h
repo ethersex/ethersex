01160bc5313b5aa06537befaa2602785df279561
Jonny Roeker <Jonny.Roeker.public@t-online.de>
2013-05-03 12:12:09 +0200

    new file:   hardware/tower/tower.h 	hardware/tower/tower.c 	hardware/tower/tower_ecmd.c

---
+++ hardware/tower/tower.h
@@ -0,0 +1,42 @@
+/*
+*
+* Copyright (c) 2009 by Jonny Roeker <dg9oaa@darc.de>
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 3
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*
+* For more information on the GPL, please go to:
+* http://www.gnu.org/copyleft/gpl.html
+*/
+
+#include <stdint.h>
+#include "config.h"
+
+#ifdef TOWERCTL_SUPPORT
+
+
+void tower_periodic(void);
+void tower_init(void);
+
+void push_button_up(uint16_t time);
+void push_button_down(uint16_t time);
+
+uint16_t timervalue;
+
+uint8_t is_button_up;
+uint8_t is_button_down;
+
+void set_power(uint8_t on);
+
+#endif
