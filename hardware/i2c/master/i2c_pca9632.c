13e15aecf5932064a0fa438e67e09de799a1fb30
beroot <beroot@beroot.de>
2013-05-12 00:12:25 +0200

    NEW: Support for PCA9632

--- hardware/i2c/master/i2c_pca9632.c
+++ hardware/i2c/master/i2c_pca9632.c
@@ -30,22 +30,49 @@
 
 #ifdef I2C_PCA9632_SUPPORT
 /*
-MODE1  
-MODE2
-LED_OUT_STATE
+Slave address of PCA9632 is 0x62h
+(8-pin packages is fixed to this address and only one chip can be 
+connected to the bus, 10-pin packages has two additional address 
+pins for up to four chips on the bus).
 
-bits
-00 = Output is set Hi-Z (LED off - default)
-01 = Output is set low (LED on)
-10 = Output blinks at PWM0 rate
-11 = Output blinks at PWM1 rate
+MODE1 (register address 0x00h)
+bit 4 (SLEEP) has to be set to 0 to start the oscillator and to be able to
+set LED PWM values.
+ 
+MODE2 (register address 0x01h) (* is default value)
+bit  value  description
+0    0*     unused
+1    1*     unused
+2    0*     output open-drain
+     1      output totem pole
+3    0*     change on STOP
+     1      change on ACK (important for synchronizing chips)
+4    0*     output NOT inverted
+     1      output inverted
+5    0*     group control = dimming
+     1      group control = blinking
+6    0*     reserved
+7    0*     reserved
+                     
+LED_OUT_STATE (register address 0x08h) (* is default value)
+bit  symbol  value
+0,1  LDR0    00*
+2,3  LDR1    00*
+4,5  LDR2    00*
+6,7  LDR3    00*
+                     
+LDRx = 00 LED driver is off
+LDRx = 01 LED driver is on
+LDRx = 10 LED driver is individually controlled by its PWM
+LDRx = 11 LED driver is additionally controlled by group dimming or blinking
 */
+                    
 uint8_t
 i2c_pca9632_reset(void)
 {
   uint8_t ret = 1;
   /* SWRST Call Address = 0b0000 011 */
-  if (! i2c_master_select(0x03, TW_WRITE))
+  if (!i2c_master_select(0x03, TW_WRITE))
     goto stop;
   TWDR = 0xA5;                  /* magic reset byte1 */
   if (i2c_master_transmit_with_ack() != TW_MT_DATA_ACK)
@@ -61,80 +88,100 @@ stop:
 }
 
 uint8_t
-i2c_pca9632_init(uint8_t address, 
-  uint8_t mode1, uint8_t mode2, uint8_t led_out_state)
+i2c_pca9632_init(uint8_t address,
+                 uint8_t mode1, uint8_t mode2, uint8_t led_out_state)
 {
   uint16_t tmp;
 
   tmp = i2c_write_byte_data(address, 0, mode1);
-  I2CDEBUG ("write MODE1 register\n");
-  if (tmp != mode1) { goto end; }
+  I2CDEBUG("write MODE1 register\n");
+  if (tmp != mode1)
+  {
+    goto end;
+  }
 
   tmp = i2c_write_byte_data(address, 1, mode2);
-  I2CDEBUG ("write MODE2 register\n");
-  if (tmp != mode2) { goto end; }
+  I2CDEBUG("write MODE2 register\n");
+  if (tmp != mode2)
+  {
+    goto end;
+  }
 
   tmp = i2c_write_byte_data(address, 8, led_out_state);
-  I2CDEBUG ("write LEDOUT state register\n");
-  if (tmp != led_out_state) { goto end; }
+  I2CDEBUG("write LEDOUT state register\n");
+  if (tmp != led_out_state)
+  {
+    goto end;
+  }
 
   return 0;
 end:
   i2c_master_stop();
-  I2CDEBUG ("Error in i2c_pca9632_init\n");
+  I2CDEBUG("Error in i2c_pca9632_init\n");
   return 1;
 }
 
-uint8_t 
+uint8_t
 i2c_pca9632_set_blink(uint8_t address, uint8_t grppwm, uint8_t grpfreq)
 {
-  uint8_t adr = 0x06; 
+  uint8_t adr = 0x06;
   // GRPPWM is set in group duty cycle control register (address 06h)
   uint16_t tmp;
 
-  I2CDEBUG ("write GRPPWM and GRPFREQ to registers\n");
-  
-  tmp = i2c_write_byte_data(address, adr, grppwm);
-  if (tmp != grppwm) { goto end; }
+  I2CDEBUG("write GRPPWM and GRPFREQ to registers\n");
 
-  adr++; // GRPFREQ is set in group frequency register (address 07h) 
+  tmp = i2c_write_byte_data(address, adr, grppwm);
+  if (tmp != grppwm)
+  {
+    goto end;
+  }
+
+  adr++;                        // GRPFREQ is set in group frequency register (address 07h) 
   tmp = i2c_write_byte_data(address, adr, grpfreq);
-  if (tmp != grpfreq) { goto end; }
+  if (tmp != grpfreq)
+  {
+    goto end;
+  }
 
   return 0;
 end:
   i2c_master_stop();
-  I2CDEBUG ("Error in i2c_pca9632_set_blink\n");
+  I2CDEBUG("Error in i2c_pca9632_set_blink\n");
   return 1;
 }
-uint8_t i2c_pca9632_set_led(uint8_t address, uint8_t led_x, uint8_t pwm_x)
+
+uint8_t
+i2c_pca9632_set_led(uint8_t address, uint8_t led_x, uint8_t pwm_x)
 {
   uint16_t tmp;
 
-  I2CDEBUG ("write PWM to LED%d register\n", led_x);
-  
-  led_x = led_x + 2; // PWM of LED0 is set in address 02h 
+  I2CDEBUG("write PWM to LED%d register\n", led_x);
+
+  led_x = led_x + 2;            // PWM of LED0 is set in address 02h 
   tmp = i2c_write_byte_data(address, led_x, pwm_x);
-  if (tmp != pwm_x) { goto end; }
+  if (tmp != pwm_x)
+  {
+    goto end;
+  }
 
   return 0;
 end:
   i2c_master_stop();
-  I2CDEBUG ("Error in i2c_pca9632_set_led\n");
+  I2CDEBUG("Error in i2c_pca9632_set_led\n");
   return 1;
 }
-  
-uint8_t i2c_pca9632_read_led(uint8_t address, uint8_t led_x)
+
+uint8_t
+i2c_pca9632_read_led(uint8_t address, uint8_t led_x)
 {
   uint8_t tmp;
 
-  I2CDEBUG ("read PWM of LED%d\n", led_x);
+  I2CDEBUG("read PWM of LED%d\n", led_x);
 
-  led_x = led_x + 2; // PWM of LED0 is set in address 02h 
+  led_x = led_x + 2;            // PWM of LED0 is set in address 02h 
   tmp = i2c_read_byte_data(address, led_x);
   return tmp;
-  
-}
-  
-#endif /* I2C_PCA9632_SUPPORT */
 
+}
+
+#endif /* I2C_PCA9632_SUPPORT */
