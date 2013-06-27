7995f180a9ab2d29d767879fc707878b95a3cd23
Johann Gail <johann.gail@gmx.de>
2013-06-27 21:41:39 +0200

    Added the correct GPL V3 with the correct character encoding

--- protocols/sip/sip.h
+++ protocols/sip/sip.h
@@ -1,29 +1,23 @@
-/*----------------------------------------------------------------------------
- Copyright:      Johann Gail  mailto: johann.gail.gmx.de
- Author:         Johann Gail
- Remarks:        
- known Problems: none
- Version:        03.06.2013
- Description:    SIP Client for VOIP Calls
-
- Modifications to fit Ethersex firmware:
- Copyright (C) 2013 by Johann Gail
-
- Dieses Programm ist freie Software. Sie k�nnen es unter den Bedingungen der 
- GNU General Public License, wie von der Free Software Foundation ver�ffentlicht, 
- weitergeben und/oder modifizieren, entweder gem�� Version 2 der Lizenz oder 
- (nach Ihrer Option) jeder sp�teren Version. 
-
- Die Ver�ffentlichung dieses Programms erfolgt in der Hoffnung, 
- da� es Ihnen von Nutzen sein wird, aber OHNE IRGENDEINE GARANTIE, 
- sogar ohne die implizite Garantie der MARKTREIFE oder der VERWENDBARKEIT 
- F�R EINEN BESTIMMTEN ZWECK. Details finden Sie in der GNU General Public License. 
-
- Sie sollten eine Kopie der GNU General Public License zusammen mit diesem 
- Programm erhalten haben. 
- Falls nicht, schreiben Sie an die Free Software Foundation, 
- Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA. 
-------------------------------------------------------------------------------*/
+/*
+ * Copyright (c) 2009 by Johann Gail <johann.gail.gmx.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * For more information on the GPL, please go to:
+ * http://www.gnu.org/copyleft/gpl.html
+ */
 
 #ifndef _SIP_H
 #define _SIP_H
