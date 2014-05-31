46d176ebffe652cb00653109f11fa86e31e8e6a9
Erik Kunze <eku@users.sourceforge.net>
2014-05-31 22:04:33 +0200

    turn off tx module after transmition move fs20 configuration to protocol section decleare pinning for new hardware

--- hardware/radio/generic/generic.h
+++ hardware/radio/generic/generic.h
@@ -25,7 +25,7 @@
 #include <stdint.h>
 
 #define ASK_TX_ENABLE
-#define ASK_TX_DISABLE
+#define ASK_TX_DISABLE PIN_CLEAR(GENERIC_ASK)
 #define ASK_TX_TRIGGER generic_ask_trigger
 
 void generic_ask_trigger(uint8_t, uint16_t);
