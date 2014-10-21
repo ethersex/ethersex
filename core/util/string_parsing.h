ac5031bcba0cee9e817981cfc995fadaf48d8804
Daniel Lindner <daniel.lindner@gmx.de>
2014-10-21 18:40:21 +0200

    Make itoa_fixpoint save: add buffer lenght param Add string parsing for fixpoint int16 and int16

--- core/util/string_parsing.h
+++ core/util/string_parsing.h
@@ -21,14 +21,22 @@
 
 #include <stdint.h>
 
-/* Takes a character strint and an place for the value, and parses the first integer
+/* Takes a character string and an place for the value, and parses the first integer
  * after all spaces. If there isn't an integer, it will return 0,
  * otherwise the number of parsed characters, including the spaces
  */
 
-uint8_t next_uint16(char *cmd, uint16_t *value);
+uint8_t next_uint16(char *cmd, uint16_t * value);
+uint8_t next_int16(char *cmd, int16_t * value);
+
+/* Takes a character string and an place for the value, and parses the first number
+* after all spaces. Number can contain decimal point. The returned integer is
+* a fixpoint integer with fixeddigits decimal places. If there isn't an integer, it will return 0,
+* otherwise the number of parsed characters, including the spaces
+*/
+
+uint8_t next_int16_fp(char *cmd, int16_t * value, uint8_t fixeddigits);
 
 /* Read hexbyte at cmd, strips all spaces before and returns consumed
    characters or 0 on error */
-uint8_t next_hexbyte(char *cmd, uint8_t *value);
-
+uint8_t next_hexbyte(char *cmd, uint8_t * value);
