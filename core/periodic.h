3f23e3fb6738b18e5c91158779ecd5f731ba5dd8
Michael Brakemeier <michael@brakemeier.de>
2014-10-29 14:58:13 +0100

    Changed Timer/Counter mode to Fast PWM

--- core/periodic.h
+++ core/periodic.h
@@ -29,55 +29,75 @@
 
 #include "config.h"
 
-#define MAX_OVERFLOW    65535UL   /* 16 bit timer/counter 1/3 max */
-#define HZ              50        /* 20ms */
+/* 16 Bit Timer/Counter settings for periodic module.
+ *
+ * Timer/Counter1/3 will be configured for Fast PWM mode, counting up from
+ * 0 to TOP. TOP is set via the Output Compare Register (OCRnA). Overflow
+ * interrupts (TOV) will be used to generate periodic milliticks.
+ * Using Fast PWM and OCRnA ensures double buffered access when writing
+ * updated TOP values.
+ *
+ * Calculate periodic timer constants from CONF_MTICKS_PER_SEC:
+ * Aim at exactness of the periodic milliticks frequency. Calculate its
+ * period time (equal to TOP) first and then deduce all other timings
+ * from this one.
+ *
+ * Formula for Fast PWM acc. to AVR datasheet:
+ * CONF_MTICKS_PER_SEC = F_CPU / (PERIODIC_PRESCALER * (1 + PERIODIC_TOP))
+ * => PERIODIC_TOP = (F_CPU / (PERIODIC_PRESCALER * CONF_MTICKS_PER_SEC)) - 1
+ */
 
-/* periodic milliticks usability check -
+/* 16 bit timer/counter 1/3 upper limit */
+#define MAX_OVERFLOW            65535UL
+/* e6 basic timer frequency 50 Hz / period time 20 ms*/
+#define HZ                      50
+
+/* Calculate required prescaler value, aim for highest timer/counter
+ * resolution, i.e. lowest possible prescaler.
+ *
+ * PERIODIC_PRESCALER = F_CPU / (CONF_MTICKS_PER_SEC * (1 + MAX_OVERFLOW))
+ */
+#if F_CPU > (CONF_MTICKS_PER_SEC * (1 + MAX_OVERFLOW))
+/* Set prescaler to divide F_CPU by 8 */
+#define PERIODIC_PRESCALER      8UL
+#define PERIODIC_SET_PRESCALER  PERIODIC_PRESCALER_8
+#else
+/* Use F_CPU without prescaler to tick the timer */
+#define PERIODIC_PRESCALER      1UL
+#define PERIODIC_SET_PRESCALER  PERIODIC_PRESCALER_1
+#endif
+
+/* Periodic milliticks per second / periodic_milliticks_isr frequency.
+ * CONF_MTICKS_PER_SEC           defined via config.in/autoconf.h
+ */
+/* Timer ticks needed for one periodic millitick. Fast PWM TOP value. */
+#define PERIODIC_TOP            ((F_CPU / (PERIODIC_PRESCALER * CONF_MTICKS_PER_SEC)) - 1)
+
+/* Periodic milliticks usability check -
  * there should be at least ~1000 CPU-Cycles per periodic millitick
  */
-#if (F_CPU / CONF_CLOCKS_PER_SEC) < 1000
-#warning *** You should either increase F_CPU or decrease
-#warning *** the tick rate to make periodic milliticks work as expected!!
+#if (F_CPU / CONF_MTICKS_PER_SEC) < 1000
+#warning *** You should either increase F_CPU or decrease the tick
+#warning *** rate to make periodic milliticks work as expected!!
 #endif
 
-/* Determine 16 Bit Timer/Counter settings.
- * TC1/3 will be configured for normal mode, counting up from
- * (MAX_OVERFLOW + 1 - (F_CPU / CLOCK_PRESCALER / HZ)) to MAX_OVERFLOW.
- * TOV interrupts will be used to generate 50Hz / 20ms intervals for timer()
- * calls from meta. OCR interrupts will be used to generate milliticks.
- * CPU_CLOCK_SUPPORT uses a simple counter on top of TOV.
- */
-#if F_CPU >= (HZ * (MAX_OVERFLOW + 1))  /* break frequency 3.2768MHz */
-/* Set prescaler to divide F_CPU by 8 */
-#define CLOCK_PRESCALER         8UL
-#define CLOCK_SET_PRESCALER     PERIODIC_PRESCALER_8
-/* Huh? F_CPU > 26.214400MHz. FIXME for xmega support */
-#if (F_CPU/CLOCK_PRESCALER) >= (HZ * (MAX_OVERFLOW + 1))
-#error Validate F_CPU! Could not determine millitick timer settings.
-#endif
-#else
-/* F_CPU < 3.2768MHz, no prescaler required */
-#define CLOCK_PRESCALER         1UL
-#define CLOCK_SET_PRESCALER     PERIODIC_PRESCALER_1
-#endif
-
-/* periodic milliticks per second / periodic_milliticks_isr frequency
- * CONF_CLOCKS_PER_SECOND       defined via config.in/autoconf.h
- */
-/* timer ticks needed for one periodic millitick */
-#define CLOCK_MILLITICKS        (F_CPU/CLOCK_PRESCALER/CONF_CLOCKS_PER_SEC)
-/* timer ticks needed for one 20ms clock tick */
-#define CLOCK_TICKS             (F_CPU/CLOCK_PRESCALER/HZ)
-/* timer ticks needed for one second */
-#define CLOCK_SECONDS           (F_CPU/CLOCK_PRESCALER)
-/* Timer starts at "zero" */
-#define PERIODIC_ZERO           ((MAX_OVERFLOW + 1) - CLOCK_TICKS)
-
-/* millitick counter */
+/* periodic milliticks counter */
 extern volatile uint16_t periodic_milliticks;
 
-/* initialize hardware timer */
+/* Initialize hardware timer. */
 void periodic_init(void);
+
+#ifdef PERIODIC_ADJUST_SUPPORT
+/**
+ * Adjust the periodic timer TOP value.
+ *
+ * @param offset +/-25% offset from PERIODIC_TOP in timer ticks,
+ *      0 means reset to PERIODIC_TOP.
+ * @return the new TOP value or 0 if PERIODIC_TOP +/-offset would
+ *      exceed the allowable range.
+ */
+uint16_t periodic_adjust_set_offset(int16_t offset);
+#endif
 
 #ifdef FREQCOUNT_SUPPORT
 void timer_expired(void);
