7dcb9dc5e75f4a076c37ef0e1bad7761bc649656
Erik Kunze <github1@erik-kunze.de>
2020-05-04 13:30:46 +0200

    MQTT module
    The static configuration of the connection data of
    the MQTT broker is from now on the standard case,
    comparable to other protocols.
    The registration of any module with the MQTT module
    is now done with the well-known Ethersex meta mechanism.
    The generated configurations are stored in the flash
    and save RAM. Registration at runtime is still possible,
    but not the preferred way.

--- protocols/mqtt/static_configuration.c
+++ protocols/mqtt/static_configuration.c
@@ -28,42 +28,39 @@
 #include "config.h"
 #include "mqtt.h"
 
-
-#ifdef MQTT_STATIC_CONF
-
-#ifndef MQTT_STATIC_CONF_SUPPLY_USERNAME
-  #define MQTT_STATIC_CONF_USER NULL
-  #define MQTT_STATIC_CONF_PASS NULL
+#ifndef MQTT_CONF_SUPPLY_USERNAME
+  #define MQTT_CONF_USER NULL
+  #define MQTT_CONF_PASS NULL
 #else
-  #ifndef MQTT_STATIC_CONF_SUPPLY_PASSWORD
-    #define MQTT_STATIC_CONF_PASS NULL
+  #ifndef MQTT_CONF_SUPPLY_PASSWORD
+    #define MQTT_CONF_PASS NULL
   #endif
 #endif
 
-#ifndef MQTT_STATIC_CONF_SUPPLY_WILL
-  #define MQTT_STATIC_CONF_WILL_TOPIC NULL
-  #define MQTT_STATIC_CONF_WILL_QOS 0
-  #define MQTT_STATIC_CONF_WILL_RETAIN_FLAG false
-  #define MQTT_STATIC_CONF_WILL_MESSAGE NULL
+#ifndef MQTT_CONF_SUPPLY_WILL
+  #define MQTT_CONF_WILL_TOPIC NULL
+  #define MQTT_CONF_WILL_QOS 0
+  #define MQTT_CONF_WILL_RETAIN_FLAG false
+  #define MQTT_CONF_WILL_MESSAGE NULL
 #else
-  #ifdef MQTT_STATIC_CONF_WILL_RETAIN
-    #define MQTT_STATIC_CONF_WILL_RETAIN_FLAG true
+  #ifdef MQTT_CONF_WILL_RETAIN
+    #define MQTT_CONF_WILL_RETAIN_FLAG true
   #else
-    #define MQTT_STATIC_CONF_WILL_RETAIN_FLAG false
+    #define MQTT_CONF_WILL_RETAIN_FLAG false
   #endif
 #endif
 
 static char const *mqtt_static_conf_autosubscribe[] = {NULL};
 static mqtt_connection_config_t mqtt_static_conf =
 {
-  .client_id = MQTT_STATIC_CONF_CLIENTID,
-  .user = MQTT_STATIC_CONF_USER,
-  .pass = MQTT_STATIC_CONF_PASS,
-  .will_topic = MQTT_STATIC_CONF_WILL_TOPIC,
-  .will_qos = MQTT_STATIC_CONF_WILL_QOS,
-  .will_retain = MQTT_STATIC_CONF_WILL_RETAIN_FLAG,
-  .will_message = MQTT_STATIC_CONF_WILL_MESSAGE,
-  .will_message_length = sizeof(MQTT_STATIC_CONF_WILL_MESSAGE) - 1,
+  .client_id = MQTT_CONF_CLIENTID,
+  .user = MQTT_CONF_USER,
+  .pass = MQTT_CONF_PASS,
+  .will_topic = MQTT_CONF_WILL_TOPIC,
+  .will_qos = MQTT_CONF_WILL_QOS,
+  .will_retain = MQTT_CONF_WILL_RETAIN_FLAG,
+  .will_message = MQTT_CONF_WILL_MESSAGE,
+  .will_message_length = sizeof(MQTT_CONF_WILL_MESSAGE) - 1,
   .auto_subscribe_topics = mqtt_static_conf_autosubscribe,
   //target_ip not set, see mqtt_set_static_conf
 };
@@ -71,11 +68,9 @@ static mqtt_connection_config_t mqtt_static_conf =
 void
 mqtt_set_static_conf(void)
 {
-  set_MQTT_STATIC_CONF_SERVER_IP(&mqtt_static_conf.target_ip);
+  set_MQTT_CONF_SERVER_IP(&mqtt_static_conf.target_ip);
   mqtt_set_connection_config(&mqtt_static_conf);
 }
-
-#endif
 
 /*
   -- Ethersex META --
